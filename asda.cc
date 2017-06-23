286f50922fbd55df342cf2ef00fc2b3e42c527c3
liao20081228 <liao20081228@126.com>
2017-06-24 01:35:00 +0800

    2017年 06月 24日 星期六 01:35:00 CST

---
+++ asda.cc
@@ -0,0 +1,8 @@
+/*************************************************************************
+    > File Name: asda.cc
+    > Author: liao20081228
+    > Mail: liao20081228@126.com 
+    > Created Time: 2017年06月24日 星期六 01时34分41秒
+ ************************************************************************/
+#include<myhead>/*myhead is a headfile defined in in the directory:/usr/include,which include all headfile we need*/
+
