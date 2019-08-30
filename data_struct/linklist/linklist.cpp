805725e759b974b40c52f1252e5418ee7d6af05e
liao20081228 <liao20081228@126.com>
2019-08-30 15:12:38 +0800

    2019年 08月 30日 星期五 15:12:38 CST

--- data_struct/linklist/linklist.cpp
+++ data_struct/linklist/linklist.cpp
@@ -1,39 +1,5 @@
 #include<iostream>
-namespace mystd
-{
-	struct node
-	{
-		int data;
-		node* next;
-	
-	};
-
-
-	class LinkList
-	{
-		node head;
-		
-	public:
-		LinkList(void)
-		{
-			head.data=0;
-			head.next=nullptr;
-		}
-		
-		bool establish(void);
-		bool establish_reverse(void);
-		bool insert(int pos, int val);
-		bool del(int pos, int& val);
-		bool del(int val);
-		int search(int val) const;
-		int search_by_order(int pos) const;
-		int search_by_val(int val) const;
-		int length(void ) const;
-		bool is_empty(void) const;
-		~LinkList(void);
-	};
-}
-
+#include"linklist.hpp"
 bool mystd::LinkList::insert(int pos, int val)
 {
 	int i=0;
