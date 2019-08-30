abd5e6fabed43bee6ba40d00f3fa56c202950048
liao20081228 <liao20081228@126.com>
2019-08-30 16:00:40 +0800

    2019年 08月 30日 星期五 16:00:40 CST

--- data_struct/linklist/linklist.cpp
+++ data_struct/linklist/linklist.cpp
@@ -3,19 +3,21 @@
 
 bool mystd::LinkList::is_empty(void) const
 {
-	return head.next;
+	return static_cast<bool>(head.next);
 }
 
 bool mystd::LinkList::insert_before(int pos, int val) 
 {
 
 	int i = 0;
-	node* p = head.next;
-        while ( i != pos && p)
+	node* p = &head;
+        while (i != pos && p->next)
 	{
 		++i;
 		p = p->next;
 	}
+	if (p->next == nullptr)
+		return false;
 
 
 }
