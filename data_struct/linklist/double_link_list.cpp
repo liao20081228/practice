95eee6acfdda1aa4dc53b470de1b9254f7011610
liao20081228 <liao20081228@126.com>
2019-08-30 18:12:31 +0800

    2019年 08月 30日 星期五 18:12:31 CST

---
+++ data_struct/linklist/double_link_list.cpp
@@ -0,0 +1,181 @@
+#include"linklist.hpp"
+#include<ciso646>
+
+bool mystd::LinkList::is_empty(void) const
+{
+	return static_cast<bool>(head.next);
+}
+
+bool mystd::LinkList::insert_before(int pos, int val) 
+{
+
+	int i = 0;
+	node* p = &head;
+        while (i != pos-1 && p->next)
+	{
+		++i;
+		p = p->next;
+	}
+	if (i != pos - 1)
+		return false;
+	node* temp = p->next;
+	p->next = new node;
+	if (p->next == nullptr)
+		return false;
+	p->next->data = val;
+	p->next->next = temp;
+	return true;
+}
+
+
+bool mystd::LinkList::insert_after(int pos, int val) 
+{
+
+	int i = 0;
+	node* p = &head;
+        while (i != pos && p->next)
+	{
+		++i;
+		p = p->next;
+	}
+	if (i != pos)
+		return false;
+	node* temp = p->next;
+	p->next = new node;
+	if (p->next == nullptr)
+		return false;
+	p->next->data = val;
+	p->next->next = temp;
+	return true;
+}
+
+
+int mystd::LinkList::length(void) const 
+{
+	int i=0;
+	const node* p = &head;
+	while(p->next)
+	{
+		++i;
+		p=p->next;
+	}
+	return i;
+}
+
+
+bool mystd::LinkList::establish_front()
+{
+	int val=0;
+	std::cin >> val;
+	while( val != -1 )
+	{
+		if (insert_after(0, val) == false)
+			return false;
+		std::cin >> val;
+	}
+	return true;
+}
+
+bool mystd::LinkList::establish_trail()
+{
+	int val=0;
+	std::cin >> val;
+	while( val != -1 )
+	{
+		if (insert_after(length(), val) == false)
+			return false;
+		std::cin >> val;
+	}
+	return true;
+}
+
+
+bool mystd::LinkList::del(int pos, int& val)
+{
+	int i=0;
+	node *p = &head;
+	while(i != pos - 1 && p->next  )
+	{
+		++i;
+		p = p->next;
+	}
+
+	if (i != pos -1)
+		return false;
+	val = p->next->data;
+	node *temp = p->next;
+	p->next=p->next->next;
+	delete temp;
+	return true;
+}
+
+bool mystd::LinkList::del(int val)
+{
+	node* p =&head;
+	while(p->next )
+	{
+		if (p->next->data == val)
+		{
+			node *temp = p->next;
+			p->next=p->next->next;
+			delete temp;	
+		}
+		else
+		{
+			p = p->next;
+		}
+	}
+	return true;
+}
+
+
+std::vector<int> mystd::LinkList::search(int val) const
+{
+	std::vector<int> vec;
+	const node* p = head.next;
+	int i=0;
+	while(p)
+	{
+		++i;
+		if (p->data == val)
+			vec.push_back(i);
+		p = p->next;	
+	}
+	return vec;
+}
+
+mystd::node* mystd::LinkList::search_by_pos(int pos ) const
+{
+	int i = 0;
+	node* p = head.next;
+	while(p)
+	{
+		++i;
+		if ( i == pos )
+			return p; 
+	}
+	return nullptr;
+}
+
+
+void mystd::LinkList::display(void) const
+{
+	node *p = head.next;
+	while(p)
+	{
+		std::cout << p->data << ",";
+		p=p->next;
+	}
+	std::cout << "\n";
+}
+
+mystd::LinkList::~LinkList(void)
+{
+		node*p = head.next;
+		while(p)
+		{
+			head.next=p->next;
+			delete p;
+			p=head.next;
+		}
+}
---
+++ data_struct/linklist/double_link_list.hpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+namespace mystd
+{
+
+	struct node
+	{
+		int data;
+		node* next;
+	};	
+
+	class LinkList
+	{
+		node head;
+	public:
+		LinkList(void)
+		{
+			head.data=0;
+			head.next=nullptr;
+			std::cout << " call constructor\n"; 
+		}
+		
+		bool establish_front(void);
+		bool establish_trail(void);
+		bool establish_reverse(void);
+		bool insert_after(int pos, int val);
+		bool insert_before(int pos, int val);
+		bool del(int pos, int& val);
+		bool del(int val);
+		std::vector<int> search(int val) const;
+		node* search_by_pos(int pos) const;
+		int length(void ) const;
+		bool is_empty(void) const;
+		void display(void) const;
+		~LinkList(void);
+	};
+}
