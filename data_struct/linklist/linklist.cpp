15f7210fff4d49fcb2878f4d3fbc2af285c8eed7
liao20081228 <liao20081228@126.com>
2019-08-30 16:24:36 +0800

    2019年 08月 30日 星期五 16:24:36 CST

--- data_struct/linklist/linklist.cpp
+++ data_struct/linklist/linklist.cpp
@@ -20,7 +20,41 @@ bool mystd::LinkList::insert_before(int pos, int val)
 		return false;
 	node* temp = p->next;
 	p->next = new node;
+	if (p->next == nullptr)
+		return false;
 	p->next->data = val;
 	p->next->next = temp;
+	return true;
+}
 
+
+bool mystd::LinkList::insert_after(int pos, int val) 
+{
+
+	int i = 0;
+	node* p = &head;
+        while (i != pos && p->next)
+	{
+		++i;
+		p = p->next;
+	}
+	if (i != pos)
+		return false;
+	node* temp = p->next;
+	p->next = new node;
+	if (p->next == nullptr)
+		return false;
+	p->next->data = val;
+	p->next->next = temp;
+	return true;
+}
+
+
+int mystd::LinkList::length(void) const 
+{
+	int i=0;
+	const node* p = &head;
+	while(p->next)
+		++i;
+	return i;
 }
