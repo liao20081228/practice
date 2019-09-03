2eb8885c1153a04cb76950ea539bf6823f776431
liao20081228 <liao20081228@126.com>
2019-09-03 11:48:21 +0800

    2019年 09月 03日 星期二 11:48:21 CST

--- cpp/class/con_and_copy-con_and_de-con.cpp
+++ cpp/class/con_and_copy-con_and_de-con.cpp
@@ -1,13 +1,42 @@
 #include<iostream>
-
+#include<string>
+#include<cstring>
 class animal
 {
 	int age;
 	char * name;
 public:
-	animal(int age , char * p):age(age),name(p)
+	//构造函数的作用
+		//1.给对象标识 
+		//2.数据成员开辟内存
+		//3.数据成员初始化
+	//1.无返回值，与类同名，可以重载，参数可带默认值，不可主动调用
+	animal(int age=0 , char * p):age(age)
 	{
-
+		name=new char[std::string(p).length()+1];
+		name=p;
+	}
+	animal(char*p )
+	{
+		name=new char[std::string(p).length()+1];
+		name=p;
 	}
 
+	//析构函数：清理动态内存（含有指针数据成员）
+	//无返回值，与类同名，不能重载，没有参数，可以主动调用
+	~animal(void)
+	{
+		if (name)
+			delete [] name;
+	}
+	//拷贝构造函数是特殊的构造函数
+	//函数按值传递、是返回值、用已有对象初始化另一个对象
+	animal(const animal & ref)
+	{
+		age=ref.age;
+		name = new char[std::string(ref.name).length()+1]();
+		strcpy(name,ref.name);
+	}
+
+	
 };
