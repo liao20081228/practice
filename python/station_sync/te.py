4b4932570d5a2cd49c7fed486025cdc8448faa1e
liao20081228 <liao20081228@126.com>
2019-09-05 19:57:19 +0800

    2019年 09月 05日 星期四 19:57:19 CST

--- python/station_sync/te.py
+++ python/station_sync/te.py
@@ -3,5 +3,8 @@ import re
 ret=re.compile("I Love")
 re_ret=ret.search("I Love you, I Love you")
 print(re_ret)
+print(re_ret.groups())
+print(re_ret[1])
 re_ret=ret.match("I Love you, I Love you")
 print(re_ret)
+print(re_ret.groups())
