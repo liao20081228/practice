eb200a63002125e6ccef72f28749420d6fef7f9a
liao20081228 <liao20081228@126.com>
2019-09-05 20:03:19 +0800

    2019年 09月 05日 星期四 20:03:19 CST

--- python/station_sync/te.py
+++ python/station_sync/te.py
@@ -1,10 +1,7 @@
 import re
 
-ret=re.compile("I Love")
-re_ret=ret.search("I Love you, I Love you")
-print(re_ret)
-print(re_ret.groups())
-print(re_ret[1])
-re_ret=ret.match("I Love you, I Love you")
-print(re_ret)
-print(re_ret.groups())
+ret=re.compile("a+(b+)")
+re_ret=ret.search("I Love you, aaaa aaabb")
+print(re_ret.group())
+print(re_ret.group(1))
+print(re_ret.group(2))
