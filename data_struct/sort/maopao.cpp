7176006ee984a27e67b0dd93a4c5715e2a23cd78
liao20081228 <liao20081228@126.com>
2019-09-07 20:42:51 +0800

    2019年 09月 07日 星期六 20:42:51 CST

--- data_struct/sort/maopao.cpp
+++ data_struct/sort/maopao.cpp
@@ -1,15 +1,4 @@
 #include<iostream>
-
-
-void bubble_sort_raw(int array[], int n)
-{
-}
-
-
-
-
-
-
 void bubble_up_sort(int array[], int n)
 {
 	int temp=0;
@@ -38,17 +27,38 @@ void bubble_up_sort(int array[], int n)
 
 void bubble_down_sort(int array[], int n)
 {
+	int temp(0);
+	bool flag(false);
+	for (int i = 0; i < n-1; ++i)
+	{
+		for (int j = 0; j < n- 1 -i; j++)
+		{
+			if (array[j]> array[j+1])
+			{
+				temp=array[j];
+				array[j]=array[j + 1];
+				array[ j + 1]=temp;
+				flag = true;
+					
+			}
+		}
+		for(int k=0;k<n;++k)
+		{
+			std::cout<< array[k] << ",";
+		}
+		std::cout<<"\n";
+		if ( !flag )
+			return;
+	}
 
+	
 }
-
-
-
-
 
 
 int main(void)
 {
 	int a[]={7,5,4,28,1,9,4};
 	bubble_up_sort(a,sizeof(a)/sizeof(int));
+	bubble_down_sort(a,sizeof(a)/sizeof(int));
 	return 0;
 }
