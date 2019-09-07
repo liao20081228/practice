3f54dd905ce5ddbf9e76f29859733d69e4ba0961
liao20081228 <liao20081228@126.com>
2019-09-07 23:00:51 +0800

    2019年 09月 07日 星期六 23:00:51 CST

--- data_struct/sort/test.cpp
+++ data_struct/sort/test.cpp
@@ -8,34 +8,34 @@
 int main(void)
 {
 	srand(time(NULL));
-	int a[100],b[100],c[100];
-	for (int i=0;i<100;++i)
+	int a[10],b[100],c[100];
+	for (int i=0;i<10;++i)
 	{
-	  a[i]=rand()%100;
-	  b[i]=rand()%100;
-	  c[i]=rand()%100;
+	  a[i]=rand()%10;
+	  b[i]=rand()%10;
+	  c[i]=rand()%10;
 	}
 
-	for (int i=0;i<100;i++)
+	for (int i=0;i<10;i++)
 		std::cout<<a[i]<<",";
 	std::cout<<"\n";
-	for (int i=0;i<100;i++)
+	for (int i=0;i<10;i++)
 		std::cout<<b[i]<<",";
 	std::cout<<"\n";
-	for (int i=0;i<100;i++)
+	for (int i=0;i<10;i++)
 		std::cout<<c[i]<<",";
 	std::cout<<"\n";
 
-	buble_down_sort(a,100);
-	direct_insert_sort(b,100);
-	binary_insert_sort(c,100);
-	for (int i=0;i<100;i++)
+	buble_down_sort(a,10);
+	direct_insert_sort(b,10);
+	binary_insert_sort(c,10);
+	for (int i=0;i<10;i++)
 		std::cout<<a[i]<<",";
 	std::cout<<"\n";
-	for (int i=0;i<100;i++)
+	for (int i=0;i<10;i++)
 		std::cout<<b[i]<<",";
 	std::cout<<"\n";
-	for (int i=0;i<100;i++)
+	for (int i=0;i<10;i++)
 		std::cout<<c[i]<<",";
 	std::cout<<"\n";
 	return 0;
