ec4783e4fc98a659f93d5e8b1e1acee68a73c198
liao20081228 <liao20081228@126.com>
2019-09-07 22:48:52 +0800

    2019年 09月 07日 星期六 22:48:52 CST

---
+++ data_struct/sort/test.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include"insert_sort/binary_insert_sort.hpp"
+#include"insert_sort/direct_sort.hpp"
+#include"exchang_sort/buble_sort.hpp"
+int main(void)
+{
+	/*function_body*/
+	return 0;
+}
