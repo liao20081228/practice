8a3db67460fc98e9b676bfb3775937239a9428dd
liao20081228 <liao20081228@126.com>
2019-09-07 22:36:52 +0800

    2019年 09月 07日 星期六 22:36:52 CST

--- data_struct/sort/insert_sort/direct_sort.hpp
+++ data_struct/sort/insert_sort/direct_sort.hpp
@@ -1,14 +1,17 @@
+//假设一个序列的前i个元素个有序，将第i+1个元素插入该队列，
+//然后将第i+2插入前面有i+1元素的有序序列
+//重复，直到最后一个元素插入，则整个序列有序
 void direct_insert_sort(int array[], int n)
 {
 	int temp = 0, i,j;
-	for (i = 1 ; i <= n-1; ++i)
+	for (i = 1 ; i < n; ++i)
 	{
 		if (array[i]<array[i-1])
 		{
 			temp=array[i];
-			for (j = i ; array[j] > temp && j > 0; --j)
+			for (j = i-1 ; array[j] > temp && j >= 0; --j)
 			{
-				array[j] =array[j-1];
+				array[j+1] =array[j];
 			}
 			array[j]=temp;
 		}
