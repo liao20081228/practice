0e2ca94f534e4f67ae0b1aa36aece5a6f11f0ef4
liao20081228 <liao20081228@126.com>
2019-09-07 22:54:51 +0800

    2019年 09月 07日 星期六 22:54:51 CST

--- data_struct/sort/test.cpp
+++ data_struct/sort/test.cpp
@@ -1,9 +1,42 @@
 #include<iostream>
+#include<ctime>
+#include<cstdlib>
+#include<cstdio>
 #include"insert_sort/binary_insert_sort.hpp"
 #include"insert_sort/direct_sort.hpp"
 #include"exchang_sort/buble_sort.hpp"
 int main(void)
 {
-	/*function_body*/
+	srand(time(NULL));
+	int a[100],b[100],c[100];
+	for (int i=0;i<100;++i)
+	{
+	  a[i]=rand()%100;
+	  b[i]=rand()%100;
+	  c[i]=rand()%100;
+	}
+
+	for (int i=0;i<100;i++)
+		std::cout<<a[i]<<",";
+	std::cout<<"\n";
+	for (int i=0;i<100;i++)
+		std::cout<<b[i]<<",";
+	std::cout<<"\n";
+	for (int i=0;i<100;i++)
+		std::cout<<c[i]<<",";
+	std::cout<<"\n";
+
+	buble_down_sort(a,100);
+	direct_insert_sort(b,100);
+	binary_insert_sort(c,100);
+	for (int i=0;i<100;i++)
+		std::cout<<a[i]<<",";
+	std::cout<<"\n";
+	for (int i=0;i<100;i++)
+		std::cout<<b[i]<<",";
+	std::cout<<"\n";
+	for (int i=0;i<100;i++)
+		std::cout<<c[i]<<",";
+	std::cout<<"\n";
 	return 0;
 }
