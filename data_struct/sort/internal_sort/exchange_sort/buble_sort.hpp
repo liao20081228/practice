0314098477703fdd896e448d4dbd45b848098f76
liao20081228 <liao20081228@126.com>
2019-09-08 16:24:38 +0800

    2019年 09月 08日 星期日 16:24:38 CST

---
+++ data_struct/sort/internal_sort/exchange_sort/buble_sort.hpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+void buble_up_sort(int array[], int n)
+{
+
+}
+
+void buble_down_sort(int array[], int n)
+{
+	int i = 0,
+	    j = 0;
+	bool flag =false;
+	for (int i = 0; i < n-1; ++i)
+		for (j = 0; j < n - 1 -i; ++j)
+		{
+
+		}
+}
