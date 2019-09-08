94b67dceb7404e8d788d386f9ef007d549cae33d
liao20081228 <liao20081228@126.com>
2019-09-08 16:12:39 +0800

    2019年 09月 08日 星期日 16:12:39 CST

--- data_struct/sort/internal_sort/insert_sort/shell_sort.hpp
+++ data_struct/sort/internal_sort/insert_sort/shell_sort.hpp
@@ -1,9 +1,25 @@
 #include<iostream>
-//一个序列a1,a2,...,an，按增量dk（d1=n/2）分割为
-//a1,a1+d,a2+2d,...a1+kd;a2,a2+d,a2+2d
+//一个序列a1,a2,...,an，按增量d分割为
+//ai,ai+d，...,ai+kd，（i是0~d-1）这样的子序列
+//对这些子序列直接插入排序
+//然后缩小增量，重复以上步骤，直到d=1
 
 void shell_sort(int array[], int n)
 {
-	int temp=0;
-	for 
+	int temp=0,d=0,i=0,j=0;
+	for (d = n / 2; d>=1; d/=2 )
+	{
+		for ( i = d;i < n;++i)
+		{
+			if (array [i] < array[i-d])
+			{
+				temp = array[i];
+				for ( j = i - d; j >= 0 && array[j] > temp; j-=d)
+				{
+					array[j+d] = array[j];
+				}
+				array[j+d]=temp;
+			}
+		}
+	}
 }
