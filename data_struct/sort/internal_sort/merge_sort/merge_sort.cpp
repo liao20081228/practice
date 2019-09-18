c25aed30312bc4c8b373e202c9c1c2c37d35e2be
liao20081228 <liao20081228@126.com>
2019-09-18 23:21:45 +0800

    2019年 09月 18日 星期三 23:21:45 CST

--- data_struct/sort/internal_sort/merge_sort/merge_sort.cpp
+++ data_struct/sort/internal_sort/merge_sort/merge_sort.cpp
@@ -1,8 +1,19 @@
 #include<string.h>
 void merge(int a[], int n,int low ,int mid ,int high)
 {
-	int* b = new int[n+1];
+	int* b = new int[n];
 	memcpy(b, a, n*sizeof(a[0]));
+	int i,j,k;
+	for( i =low ,j=mid+1,k=i; i<=low && j <= high;k++)
+		if (b[i] < b[j])
+			a[k]=b[i++];
+		else
+			a[k]=b[j++];
+	while(i <=mid) 
+		a[k++]=b[i++];
+	while(j<=high)
+		a[k++]=b[j++];
+	delete[] b;
 }
 void merge_sort(int a[],int n,int low, int high)
 {
@@ -13,4 +24,11 @@ void merge_sort(int a[],int n,int low, int high)
 		merge_sort(a,n,mid+1, high);
 		merge(a,n,low,mid,high);
 	}
+}
+
+
+int main()
+{
+	int a[]={1,3,9,7,7,2,6,8,11,10};
+	merge_sort(a,10,0,9);
 }
