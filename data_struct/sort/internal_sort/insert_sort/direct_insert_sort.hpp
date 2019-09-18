3baa9bfca8e25835fa947afbc3483460ee2653c1
liao20081228 <liao20081228@126.com>
2019-09-18 21:33:54 +0800

    2019年 09月 18日 星期三 21:33:54 CST

--- data_struct/sort/internal_sort/insert_sort/direct_insert_sort.hpp
+++ data_struct/sort/internal_sort/insert_sort/direct_insert_sort.hpp
@@ -3,6 +3,7 @@
 //假设n个元素中前i个（i最初为1）有序，则只需把第i+1个元素插入前面的i个有序元素中，
 //同时插入的位置之后的元素向后移动，则前i+1个元素也是有序的
 //重复直到所有元素都有序
+//时间复杂度O(n) 平均O(n^2) 最坏O(n^2) ,空间复杂度O(1) ，稳定
 void direct_insert_sort(int array[], int n)
 {
 	int temp = 0,
@@ -18,7 +19,6 @@ void direct_insert_sort(int array[], int n)
 				array[j+1] = array[j];
 			}//array[j]是要插入位置的前一个位置
 			array[j+1] = temp;
-
 		}
 	}
 }
