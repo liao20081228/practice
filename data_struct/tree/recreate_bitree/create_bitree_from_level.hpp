3f2c933a6073a1c26ebe36ba9fd1204fe19f3afe
liao20081228 <liao20081228@126.com>
2019-09-18 18:33:48 +0800

    2019年 09月 18日 星期三 18:33:48 CST

--- data_struct/tree/recreate_bitree/create_bitree_from_level.hpp
+++ data_struct/tree/recreate_bitree/create_bitree_from_level.hpp
@@ -1,7 +1,27 @@
 #include<iostream>
 
-struct node
+struct tree_node
 {
 	int data;
-	int 
+	tree_node * left;
+        tree_node * right;
+};
+typedef tree_node tree;
+
+struct queue_node
+{
+	tree_node* p;
+	queue_node* next;
+};
+
+struct queue
+{
+	queue_node * front;
+	queue_node * rear;
+};
+
+
+void init_queue(queue & Q)
+{
+	Q.
 }
