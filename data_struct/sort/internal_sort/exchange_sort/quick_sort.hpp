ff35d4c08f9b984b808ac3324b7b59c8da8d2051
liao20081228 <liao20081228@126.com>
2019-09-18 22:03:49 +0800

    2019年 09月 18日 星期三 22:03:49 CST

--- data_struct/sort/internal_sort/exchange_sort/quick_sort.hpp
+++ data_struct/sort/internal_sort/exchange_sort/quick_sort.hpp
@@ -1,3 +1,5 @@
+#include<iostream>
+
 void quick_sort(int a[], int low , int high )
 {
 	if (low < high)
@@ -18,4 +20,14 @@ void quick_sort(int a[], int low , int high )
 		quick_sort(a,low,i-1);
 		quick_sort(a,i+1,high);
 	}
+}
+
+
+int main(void)
+{
+	int a[]={12,1,2,5,4,8,7,3};
+	quick_sort(a,0,7);
+	for(int e:a)
+		std::cout<<e<<",";
+	return 0;
 }
