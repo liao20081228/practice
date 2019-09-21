9a6669453bbb6cdedc6095a09db662cb25f5162c
liao20081228 <liao20081228@126.com>
2019-09-21 11:15:41 +0800

    2019年 09月 21日 星期六 11:15:41 CST

--- data_struct_and_algorithm/sort/internal_sort/insert_sort/shell_sort.cpp
+++ data_struct_and_algorithm/sort/internal_sort/insert_sort/shell_sort.cpp
@@ -1,17 +1,24 @@
-void shell_sort(int a[], int n)
-{
-	int i,j,temp,d;
-	for (d = n/2; d >= 1; d/=2)
-	{
-		for ( i = d; i < n ; ++i)
-		{
-			if (a[i] < a[i-d])
-			{
-				temp = a[i];
-				for (j = i-d ; j>=0 && a[j] > temp; j-=d)
-				{
+#include<iostream>
+//一个序列A1,A2,...,An，按增量d分割为[A1,A2,...,Ad-1],[Ad,Ad+1,...,A2d-1],...
+//对于 Ai,Ai+d，...,Ai+kd，（i是0~d-1）这样的子序列
+//对这些子序列直接插入排序
+//然后缩小增量，重复以上步骤，直到d=1
 
+void shell_sort(int array[], int n)
+{
+	int temp=0,d=0,i=0,j=0;
+	for (d = n / 2; d>=1; d/=2 )
+	{
+		for ( i = d;i < n;++i)
+		{
+			if (array [i] < array[i-d])
+			{
+				temp = array[i];
+				for ( j = i - d; j >= 0 && array[j] > temp; j-=d)
+				{
+					array[j+d] = array[j];
 				}
+				array[j+d]=temp;
 			}
 		}
 	}
