8b38f0fe45ea77375d2e100190851cf24868ddb5
liao20081228 <liao20081228@126.com>
2019-09-21 14:51:01 +0800

    2019年 09月 21日 星期六 14:51:01 CST

--- data_struct_and_algorithm/algorithm/dp/lcs/lcs.cpp
+++ data_struct_and_algorithm/algorithm/dp/lcs/lcs.cpp
@@ -9,19 +9,28 @@ int main(void)
 	std::cin>>vec1;
 	std::cin>>vec2;
 	int dp[1000][1000]={};
+	int direct[1000][1000]={};
 	for (int i=1; i <= (int)vec1.size();++i)
 
 		for(int j=1; j <= (int)vec2.size();++j)
 		{
 			if ( vec1[i-1] == vec2[j-1] )
+			{
 				dp[i][j]=dp[i-1][j-1]+1;
+				direct[i][j]=0;
+
+			}
 			else
+			{
 				dp[i][j]=dp[i-1][j]>dp[i][j-1]?dp[i-1][j]:dp[i][j-1];
+				direct[i][j]=dp[i-1][j]>dp[i][j-1]?1:2;
+				
+			}
 		}
 	
 	std::cout<<  dp[vec1.size()][vec2.size()];
-
-
+	
+	for
 
 	return 0;
 }
