be11ec2cac966ead1b78eeb7447e6ec8ba71695f
liao20081228 <liao20081228@126.com>
2019-09-21 11:03:39 +0800

    2019年 09月 21日 星期六 11:03:39 CST

---
+++ data_struct_and_algorithm/sort/sort_alogrithm.hpp
@@ -0,0 +1,8 @@
+void direct_insert_sort(int a[], int n);
+void binary_insert_sort(int a[], int n);
+void shell_sort(int a[], int n);
+void buble_sort(int a[], int n);
+void quick_sort(int a[], int n);
+void simple_insert_sort(int a[], int n);
+void heap_sort(int a[], int n);
+void merge_sort(int a[], int n);
