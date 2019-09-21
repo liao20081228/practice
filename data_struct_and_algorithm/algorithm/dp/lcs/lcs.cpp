38c7c7b6706994d90908f533cff8eb0ad11797bf
liao20081228 <liao20081228@126.com>
2019-09-21 14:33:02 +0800

    2019年 09月 21日 星期六 14:33:02 CST

--- data_struct_and_algorithm/algorithm/dp/lcs/lcs.cpp
+++ data_struct_and_algorithm/algorithm/dp/lcs/lcs.cpp
@@ -4,18 +4,14 @@
 int main(void)
 {
 	using std::vector;
-	vector<int> vec1;
-	vector<int> vec2;
-	int temp=0;
-	while(std::cin>>temp)
-		vec1.push_back(temp);
-	while(std::cin>>temp)
-		vec2.push_back(temp);
+	std::string vec1;
+	std::string vec2;
+	std::cin>>vec1;
+	std::cin>>vec2;
 	int dp[1000][1000]={};
+	for (int i=1; i <= (int)vec1.size();++i)
 
-	for (int i=1; i < (int)vec1.size();++i)
-
-		for(int j=1; j < (int)vec2.size();++j)
+		for(int j=1; j <= (int)vec2.size();++j)
 		{
 			if ( vec1[i-1] == vec2[j-1] )
 				dp[i][j]=dp[i-1][j-1]+1;
@@ -23,7 +19,7 @@ int main(void)
 				dp[i][j]=dp[i-1][j]>dp[i][j-1]?dp[i-1][j]:dp[i][j-1];
 		}
 	
-	std::cout<< vec1.size() - dp[vec1.size()-1][vec2.size()-1];
+	std::cout<<  dp[vec1.size()-1][vec2.size()-1];
 
 
 
