e966e187621e61bceeea6cf8a87e7e11fc4a0c90
liao20081228 <liao20081228@126.com>
2019-10-07 18:15:39 +0800

    2019年 10月 07日 星期一 18:15:39 CST

--- data_struct/sort/internal_sort/select_sort/heap_sort.cpp
+++ data_struct/sort/internal_sort/select_sort/heap_sort.cpp
@@ -1,12 +1,26 @@
 #include<iostream>
 
-void build_max_heap(int a[], int n)
+void adjust_down(int a[], int k, int n)
 {
 
+}
+void build_max_heap(int a[], int n)
+{
+	for(int i=n/2;i > 0 ; i--)
+	{
+		adjust_down(a,i,n);
+	}
 }
 void heap_sort(int a[], int n)
 {
 	build_max_heap(a,n);
+	for(int i =n; i>1;i--)
+	{
+		a[0]=a[1];
+		a[1]=a[i];
+		a[i]=a[0];
+		adjust_down(a,1,i-1);
+	}
 }
 
 
