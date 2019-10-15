afcbb51a99166711e9a14306fcd20a4d791b3d87
liao20081228 <liao20081228@126.com>
2019-10-15 17:36:39 +0800

    2019年 10月 15日 星期二 17:36:39 CST

--- data_struct_and_algorithm/data_struct/list/sqlist/sqList.hpp
+++ data_struct_and_algorithm/data_struct/list/sqlist/sqList.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #ifndef MY_SQLIST_H
 #define MY_SQLIST_H
+#include<stdlib.h>
 template<typename T>
 class sqList
 {
@@ -12,10 +13,10 @@ public:
 	~sqList(void);
 
 	bool resize(int n);//重新分配大小
-	bool insert(int pos, T e);//在位置pos上插入e
+	bool insert(T e, int pos);//在位置pos上插入e
 	bool del(int pos, T & e);//删除位置pos的 元素
 	bool del(T e);//删除值为e的元素
-	bool clear(void);//清空元素
+	void clear(void);//清空元素
 	int  size(void) const;//返回元素个数
 	bool empty(void) const;//为空
 	bool full(void) const;//为满
@@ -26,10 +27,30 @@ public:
 template<typename T>
 sqList<T>::sqList(int n):data(new T[n]()),capacity(n),length(0)
 {
+	if (!data)
+	exit(-1);
+}
+
+template<typename T>
+void sqList<T>::clear(void)
+{
+	memset(data, 0, sizeof(T)*capacity);
+}
+
+
+template<typename T>
+bool sqList<T>::resize(int n)
+{
 
 }
 
 
+
+template<typename T>
+bool sqList<T>::insert(int pos ,int e)
+{
+	
+}
 
 
 
