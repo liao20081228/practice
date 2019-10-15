ea2a75eff9df63c10fa868080a8eb33c3c150deb
liao20081228 <liao20081228@126.com>
2019-10-15 18:00:41 +0800

    2019年 10月 15日 星期二 18:00:41 CST

--- data_struct_and_algorithm/data_struct/list/sqlist/sqList.hpp
+++ data_struct_and_algorithm/data_struct/list/sqlist/sqList.hpp
@@ -20,7 +20,7 @@ public:
 	int  size(void) const;//返回元素个数
 	bool empty(void) const;//为空
 	bool full(void) const;//为满
-	bool find(T e, int & pos) const;//返回第一个e出现的位置
+	int find(T e) const;//返回第一个e出现的位置
 	void display(void)const;//显示所有元素
 };
 
@@ -60,7 +60,7 @@ bool sqList<T>::resize(int n)
 template<typename T>
 bool sqList<T>::insert(T e, int pos)
 {
-	if(pos>=length+2)
+	if(pos>=length+2 || pos <1)
 		return false;
 	if(capacity == length && !resize(capacity*2))
 		return false;
@@ -70,11 +70,21 @@ bool sqList<T>::insert(T e, int pos)
 	return true;
 }
 
-
-
-
-
-
+template<typename T>
+bool sqList<T>::del(int pos, T&e)
+{
+	if (pos >= length || pos < 1)
+		return false;
+	e=data[pos-1];
+	for(int i=pos-1;i<length-1;++i)
+		data[i]=data[i+1];
+	return true;
+}
+template<typename T>
+bool sqList<T>::del(T e)
+{
+	
+}
 
 
 
