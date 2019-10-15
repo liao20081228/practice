37024d23796166e8e7902cda1912c669b591119f
liao20081228 <liao20081228@126.com>
2019-10-15 14:06:41 +0800

    2019年 10月 15日 星期二 14:06:41 CST

---
+++ data_struct_and_algorithm/sort/internal_sort/insert_sort/shell_sort.h
@@ -0,0 +1,27 @@
+#pragma once
+#ifndef MY_SHELL_SORT_H
+#define MY_SHELL_SORT_H
+#endif /* end of include guard: MY_SHELL_SORT_H */
+
+void shell_sort(int a[], int n)
+{
+	int i = 0,
+	    j = 0,
+	    d = 0,
+	    temp = 0;
+	for( d = n/2; d >= 1; d/=2 )
+	{
+		for(i = d; i < n; ++i)
+		{
+			if( a[i] < a[i - d] )
+			{
+				temp = a[i];
+				for(j = i - d; j >= 0 && a[j] > temp; j-=d )
+					a[j + d] = a[j];
+				a[j + d] =temp;
+			}
+
+		}
+	}
+}
+
