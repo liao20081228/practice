f5a0b3405eb8588274307c95f8dbe03f59c4966a
liao20081228 <liao20081228@126.com>
2019-10-15 11:42:40 +0800

    2019年 10月 15日 星期二 11:42:40 CST

--- data_struct_and_algorithm/sort/internal_sort/exchange_sort/quick_sort.h
+++ data_struct_and_algorithm/sort/internal_sort/exchange_sort/quick_sort.h
@@ -2,3 +2,24 @@
 #ifndef MY_QUICK_SORT_H
 #define MY_QUICK_SORT_H
 #endif /* end of include guard: MY_QUICK_SORT_H */
+
+void quick_sort(int a[], int low, int high)
+{
+	if(low >= high)
+		return ;
+	int i = low,
+	j = high,
+	pos = a[low];
+	while(i < j)
+	{
+		while(i < j && a[j] >= pos) 
+			--j;
+		a[i] = a[j];
+		while( i < j && a[i] <= pos)
+			++i;
+		a[j] =  a[i];
+	}
+	a[i] = pos;
+	quick_sort(a, i + 1, high);
+	quick_sort(a, low, i - 1);
+}
