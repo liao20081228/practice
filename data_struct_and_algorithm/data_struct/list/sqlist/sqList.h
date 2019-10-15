e7c61dd62f28bf4d188bb8c468981d09f968367c
liao20081228 <liao20081228@126.com>
2019-10-15 15:48:45 +0800

    2019年 10月 15日 星期二 15:48:45 CST

--- data_struct_and_algorithm/data_struct/list/sqlist/sqList.h
+++ data_struct_and_algorithm/data_struct/list/sqlist/sqList.h
@@ -12,25 +12,24 @@ class sqList
 	int length;
 	
 	public:
-		sqList(int size= 5):size(size)
-		{
-			List=new int[size]();
-			if (List == nullptr)
-			{
-				throw std::invalid_argument("nullptr");
-			}
-		}
+		sqList(int size);
 		~sqList(void);
-
 		bool  initialize(void);
-		bool  insert(int pos, int val );
+
+		bool  insert(int pos, int val);
+
 		bool  del(int pos, int* ret);
+
 		bool  del(int val);
+
 		bool  clear(void);
+
 		void  display (void ) const;
-		std::vector<int>   location(int val) const;
+
 		bool  is_empty(void) const;
+
 		bool  is_full(void) const;
+
 		int   length(void) const;
 };
 } // namespace mystd
