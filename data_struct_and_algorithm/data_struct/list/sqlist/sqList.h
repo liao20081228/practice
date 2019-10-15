c797931358091d23a625bd82962cbab716775390
liao20081228 <liao20081228@126.com>
2019-10-15 17:12:40 +0800

    2019年 10月 15日 星期二 17:12:40 CST

--- data_struct_and_algorithm/data_struct/list/sqlist/sqList.h
+++ data_struct_and_algorithm/data_struct/list/sqlist/sqList.h
@@ -11,15 +11,16 @@ public:
 	sqList(int n=1);
 	~sqList(void);
 
-	bool resize(int n);
-	bool insert(int pos, T e, bool direction);
-	bool del(int pos, T & e);
-	bool del(T e);
-	bool clear(void);
-	int size(void) const;
-	bool empty(void) const;
-	bool full(void) const;
-	bool find(T e, int & pos) const;
+	bool resize(int n);//重新分配大小
+	bool insert(int pos, T e);//在位置pos上插入e
+	bool del(int pos, T & e);//删除位置pos的 元素
+	bool del(T e);//删除值为e的元素
+	bool clear(void);//清空元素
+	int  size(void) const;//返回元素个数
+	bool empty(void) const;//为空
+	bool full(void) const;//为满
+	bool find(T e, int & pos) const;//返回第一个e出现的位置
+	void display(void)const;//显示所有元素
 };
 
 
