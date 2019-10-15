4bd602d2ae74bc895a82feb873216fdf50e7e3ea
liao20081228 <liao20081228@126.com>
2019-10-15 11:12:43 +0800

    2019年 10月 15日 星期二 11:12:43 CST

---
+++ data_struct_and_algorithm/sort/internal_sort/exchange_sort/buble_sort.h
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+
+void buble_sort(int* a, int n)
+{
+	int i = 0, 
+	    j = 0,
+	    flag = 0,
+	    temp = 0;
+	for(i = 0; i < n-1; ++i)
+	{
+		flag = 0;
+		for(j = 0; j < n - 1 - i; ++j)
+		{
+			if(a[j] > a[j + 1])
+			{
+				temp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = temp;
+				flag = 1;
+			}
+		}
+		if (!flag)
+			return;
+	}	
+}
