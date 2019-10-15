e2a9172aa95054c402106dd47f20b097c82d8b20
liao20081228 <liao20081228@126.com>
2019-10-15 14:24:43 +0800

    2019年 10月 15日 星期二 14:24:43 CST

--- data_struct_and_algorithm/sort/internal_sort/select_sort/heap_sort.h
+++ data_struct_and_algorithm/sort/internal_sort/select_sort/heap_sort.h
@@ -4,8 +4,29 @@
 
 #endif /* end of include guard: MY_HEAP_SORT_H */
 
+
+void adjust_down(int a[], int m ,int k)
+{
+
+}
+
+
+
+void build_max_heap(int a[], int n)
+{
+	for(int i = n/2; i > 0; --i)
+		adjust_down(a, n, i);
+}
+
+
 void heap_sort(int a[] , int n)
 {
 	build_max_heap(a, n);
-	for(int i = i-1;)
+	for(int i = n; i > 1;--i)
+	{
+		a[0] = a[i];
+		a[i] = a[1];
+		a[1] = a[0];
+		adjust_down(a, n, 1);
+	}
 }
