c09a18fd4b72f4ff034e5e94c6e3022caab66d99
liao20081228 <liao20081228@126.com>
2019-10-15 16:06:46 +0800

    2019年 10月 15日 星期二 16:06:46 CST

--- data_struct_and_algorithm/data_struct/list/sqlist/sqList.h
+++ data_struct_and_algorithm/data_struct/list/sqlist/sqList.h
@@ -2,43 +2,14 @@
 #ifndef MY_SQLIST_H
 #define MY_SQLIST_H
 
-#include<cstring>
-#include<iostream>
-#include<exception>
-#include<random>
-#include<vector>
 
 
-namespace mystd
-{
-class sqList
-{
-	int* List;
-	int capacity;
-	int length;
-	
-	public:
-		sqList(int size);
-		~sqList(void);
 
-		bool  initialize(void);
 
-		bool  insert(int pos, int val);
 
-		bool  del(int pos, int* ret);
 
-		bool  del(int val);
 
-		bool  clear(void);
 
-		void  display (void ) const;
 
-		bool  is_empty(void) const;
-
-		bool  is_full(void) const;
-
-		int   size(void) const;
-};
-} // namespace mystd
 
 #endif /* end of include guard: MY_SQLIST_H */
