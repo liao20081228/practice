4a421c7766913061155f1cf6df30dd05485a05b2
liao20081228 <liao20081228@126.com>
2019-10-15 14:54:45 +0800

    2019年 10月 15日 星期二 14:54:45 CST

---
+++ data_struct_and_algorithm/sort/internal_sort/merge_sort/merge_sort.h
@@ -0,0 +1,23 @@
+#pragma once
+#ifndef MY_MERGE_SORT_H
+#define MY_MERGE_SORT_H
+
+#endif /* end of include guard: MY_MERGE_SORT_H */
+
+void merge(int a[], int n, int low, int mid, int high)
+{
+
+}
+
+
+
+void merge_sort(int a[], int n, int low , int high)
+{
+	if( low < high )
+	{
+		int mid = (low + high) / 2;
+		merge_sort(a, n, low, mid );
+		merge_sort(a, n, mid + 1, high);
+		merge(a, n, low, mid, high);
+	}
+}
