fad9085e8784178b83f16cb7e86df0d9d7925908
liao20081228 <liao20081228@126.com>
2019-10-15 13:30:44 +0800

    2019年 10月 15日 星期二 13:30:44 CST

---
+++ data_struct_and_algorithm/sort/internal_sort/insert_sort/direct_insert_sort.h
@@ -0,0 +1,21 @@
+#pragma once
+#ifndef MY_DIRECT_INSERT_SORT_H
+#define MY_DIRECT_INSERT_SORT_H
+#endif /* end of include guard: MY_DIRECT_INSERT_SORT_H */
+
+void direct_insert_sort(int a[], int n)
+{
+	int i = 0,
+	    j = 0,
+	    temp = 0;
+	for(i = 1;i < n; ++i)
+	{
+		if(a[i] < a[i - 1])
+		{
+			temp = a[i];
+			for(j = i - 1; j >= 0 && a[j] < temp; --j)
+				a[j + 1] = a[j];
+			a[j+1] = temp;
+		}
+	}
+}
