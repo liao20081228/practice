d5c83731600db7a706bfe786c795083319a9d942
liao20081228 <liao20081228@126.com>
2019-12-09 22:51:43 +0800

    2019年 12月 09日 星期一 22:51:43 CST

--- rdma/vpi.c
+++ rdma/vpi.c
@@ -32,6 +32,17 @@ int main(void)
 		printf("open device failed\n");
 		exit(-1);
 	}
+	
+	struct ibv_device_attr * ib_dev_attr=(struct ibv_device_attr*)malloc(sizeof(struct ibv_device_attr));
+	assert(0==ibv_query_device(ib_dev_context,ib_dev_attr));
+	printf("fw_ver is : %s\n ",ib_dev_attr->fw_ver);
+	printf("node_guid is : %lld\n ",ib_dev_attr->node_guid);
+	printf("sys_image_guid is : %lld\n ",ib_dev_attr->sys_image_guid);
+	printf("max_mr_size is : %ld\n ",ib_dev_attr->max_mr_size);
+	printf(" is : %\n ",ib_dev_attr->);
+
+	free(ib_dev_attr);
+	ib_dev_attr=NULL;
 
 	
 	assert(0==ibv_close_device(ib_dev_context));
