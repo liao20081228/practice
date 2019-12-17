451e8c6a4e204d41783c82d04e07b199678d5f80
liao20081228 <liao20081228@126.com>
2019-12-17 19:45:47 +0800

    2019年 12月 17日 星期二 19:45:47 CST

--- rdma/common.c
+++ rdma/common.c
@@ -2,7 +2,6 @@
 #include<stdio.h>
 int main(int argc, char** argv)
 { 
-	opterr=0;	
         const char* opstring="m:n::o";
         char ret=0;
         printf("optind is %d, optarg is %s, %p, opterr is %d, optopt is %c, %d, return value is %c, %d\n",optind,optarg,optarg,opterr,optopt,optopt,ret,ret);
