ecd281c717a60e1b05eb779c0cbb05262e9e2ec7
liao20081228 <liao20081228@126.com>
2019-12-17 15:51:49 +0800

    2019年 12月 17日 星期二 15:51:48 CST

--- rdma/rdmacm.c
+++ rdma/rdmacm.c
@@ -2,4 +2,12 @@
 #include<rdma/rdma_verbs.h>
 #include<rdma/rdma_cma.h>
 
+int main(void)
+{
+	struct rdma_event_channel*  rec=rdma_create_event_channel();
+	
+	rdma_destroy_event_channel(rec);
+	 
+	return 0;
+}
 
