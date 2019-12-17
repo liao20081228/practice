b91706e17452a52b8814ff9d33705019b45a5305
liao20081228 <liao20081228@126.com>
2019-12-17 19:21:53 +0800

    2019年 12月 17日 星期二 19:21:53 CST

--- rdma/common.c
+++ rdma/common.c
@@ -1,9 +1,17 @@
 #include"common.h"
 #include<stdio.h>
-void main(int argc, char** argv)
-{
-	const char* opstring="a:c:s";
-	printf("optind is %d, optarg is %s, %p, opterr is %d, optopt is %c, %d",optind,optarg,optarg,opterr,optopt,optopt);
-	getopt(argc,argv,opstring);
-}
-
+int main(int argc, char** argv)
+{        
+        const char* opstring="m:n::o";
+        char ret=0;
+        printf("optind is %d, optarg is %s, %p, opterr is %d, optopt is %c, %d, return value is %c, %d\n",optind,optarg,optarg,opterr,optopt,optopt,ret,ret);
+        ret=getopt(argc,argv,opstring);
+        printf("optind is %d, optarg is %s, %p, opterr is %d, optopt is %c, %d, return value is %c, %d\n",optind,optarg,optarg,opterr,optopt,optopt,ret,ret);
+        ret=getopt(argc,argv,opstring);                                                                                                                                                    
+        printf("optind is %d, optarg is %s, %p, opterr is %d, optopt is %c, %d, return value is %c, %d\n",optind,optarg,optarg,opterr,optopt,optopt,ret,ret);
+        ret=getopt(argc,argv,opstring);
+        printf("optind is %d, optarg is %s, %p, opterr is %d, optopt is %c, %d, return value is %c, %d\n",optind,optarg,optarg,opterr,optopt,optopt,ret,ret);
+        ret=getopt(argc,argv,opstring);
+        printf("optind is %d, optarg is %s, %p, opterr is %d, optopt is %c, %d, return value is %c, %d\n",optind,optarg,optarg,opterr,optopt,optopt,ret,ret);
+}        
+   
