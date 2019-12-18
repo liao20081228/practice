b873759b2c20fbdfe775bb927d46a517eb812d1e
liao20081228 <liao20081228@126.com>
2019-12-18 23:15:35 +0800

    2019年 12月 18日 星期三 23:15:35 CST

--- rdma/common.c
+++ rdma/common.c
@@ -1,7 +1,16 @@
 #include"common.h"
 
 
-struct options
+struct options longopt[]=
+{
+	{"help", no_argument, NULL, 'H'},
+	{"version", no_argument, NULL, 'V'},
+	{"server", no_argument, NULL, 's'},
+	{"client",no _argument, NULL, 'c'},
+	{"port",required_argument, NULL, 'p'},
+	{"address",required_argument, NULL, 'a'},
+	{0,0,0,0,}
+};
 
 void handle_cmd(int argc, char* argv[])
 {
