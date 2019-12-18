63cb64c597c13079dba8bba8b67215a96371e879
liao20081228 <liao20081228@126.com>
2019-12-18 20:15:45 +0800

    2019年 12月 18日 星期三 20:15:45 CST

--- rdma/common.c
+++ rdma/common.c
@@ -14,23 +14,23 @@ int main(int argc, char** argv)
 	
         const char* opstring="-m:n::o";
         char ret=0;
-        printf("0                        optind is %d,             optarg is %s,      opterr is %d,           optopt is %c, %d,       return value is %c, %d\n",optind,optarg,opterr,optopt,optopt,ret,ret);
+        printf("0       optind is %d,             optarg is %s,      opterr is %d,           optopt is %c, %d,       return value is %c, %d,",optind,optarg,opterr,optopt,optopt,ret,ret);
         fun(argc,argv);
 	
 	ret=getopt(argc,argv,opstring);
-        printf("1                        optind is %d,             optarg is %s,      opterr is %d,           optopt is %c, %d,       return value is %c, %d\n",optind,optarg,opterr,optopt,optopt,ret,ret);
+        printf("0       optind is %d,             optarg is %s,      opterr is %d,           optopt is %c, %d,       return value is %c, %d,",optind,optarg,opterr,optopt,optopt,ret,ret);
         fun(argc,argv);
         
 	ret=getopt(argc,argv,opstring);                                                                                                                                                    
-        printf("2                        optind is %d,             optarg is %s,      opterr is %d,           optopt is %c, %d,       return value is %c, %d\n",optind,optarg,opterr,optopt,optopt,ret,ret);
+        printf("0       optind is %d,             optarg is %s,      opterr is %d,           optopt is %c, %d,       return value is %c, %d,",optind,optarg,opterr,optopt,optopt,ret,ret);
         fun(argc,argv);
         
 	ret=getopt(argc,argv,opstring);
-        printf("3                        optind is %d,             optarg is %s,      opterr is %d,           optopt is %c, %d,       return value is %c, %d\n",optind,optarg,opterr,optopt,optopt,ret,ret);
+        printf("0       optind is %d,             optarg is %s,      opterr is %d,           optopt is %c, %d,       return value is %c, %d,",optind,optarg,opterr,optopt,optopt,ret,ret);
         fun(argc,argv);
         
 	ret=getopt(argc,argv,opstring);
-        printf("4                        optind is %d,             optarg is %s,      opterr is %d,           optopt is %c, %d,       return value is %c, %d\n",optind,optarg,opterr,optopt,optopt,ret,ret);
+        printf("0       optind is %d,             optarg is %s,      opterr is %d,           optopt is %c, %d,       return value is %c, %d,",optind,optarg,opterr,optopt,optopt,ret,ret);
         fun(argc,argv);
 }        
    
