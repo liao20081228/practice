696d3655ec4841b57deeb95b84402b5d4d097a39
liao20081228 <liao20081228@126.com>
2019-12-18 23:09:38 +0800

    2019年 12月 18日 星期三 23:09:38 CST

--- rdma/common.c
+++ rdma/common.c
@@ -1,36 +1,10 @@
 #include"common.h"
-#include<stdio.h>
-void fun(int argc, char**argv)
-{
-	printf("argv is :");
-	for(int i=0;i<argc;++i)
-		printf("%s,  ", argv[i]);
-		printf("\n");
-		printf("--------------------------------------------------------------------------------------------\n");
 
+
+struct options
+
+void handle_cmd(int argc, char* argv[])
+{
+	
 }
-int main(int argc, char** argv)
-{ 
-	
-        const char* opstring="m";
-        char ret=0;
-        printf("0\toptind is %d,\toptarg is %s,\topterr is %d,\tptopt is %c, %d,\treturn value is %c, %d,\t",optind,optarg,opterr,optopt,optopt,ret,ret);
-        fun(argc,argv);
-	
-	ret=getopt(argc,argv,opstring);
-        printf("0\toptind is %d,\toptarg is %s,\topterr is %d,\tptopt is %c, %d,\treturn value is %c, %d,\t",optind,optarg,opterr,optopt,optopt,ret,ret);
-        fun(argc,argv);
-        
-	ret=getopt(argc,argv,opstring);                                                                                                                                                    
-        printf("0\toptind is %d,\toptarg is %s,\topterr is %d,\tptopt is %c, %d,\treturn value is %c, %d,\t",optind,optarg,opterr,optopt,optopt,ret,ret);
-        fun(argc,argv);
-        
-	ret=getopt(argc,argv,opstring);
-        printf("0\toptind is %d,\toptarg is %s,\topterr is %d,\tptopt is %c, %d,\treturn value is %c, %d,\t",optind,optarg,opterr,optopt,optopt,ret,ret);
-        fun(argc,argv);
-        
-	ret=getopt(argc,argv,opstring);
-        printf("0\toptind is %d,\toptarg is %s,\topterr is %d,\tptopt is %c, %d,\treturn value is %c, %d,\t",optind,optarg,opterr,optopt,optopt,ret,ret);
-        fun(argc,argv);
-}        
    
