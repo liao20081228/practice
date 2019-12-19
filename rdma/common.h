6fe1de2b9ed486544b921b094ed8d7b57d2ce60c
liao20081228 <liao20081228@126.com>
2019-12-19 14:39:44 +0800

    2019年 12月 19日 星期四 14:39:44 CST

--- rdma/common.h
+++ rdma/common.h
@@ -1,15 +1,11 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<getopt.h>
 
 #pragma once
 #ifndef RDMA_COMMON_H
-#define RDMA_COMMON_H
-
-
-
-
-
+	#define RDMA_COMMON_H
+#include<stdio.h>
+#include<stdlib.h>
+#include<getopt.h>
+#include"version.h"
 
 
 
---
+++ rdma/version.h
@@ -0,0 +1,26 @@
+
+#pragma once
+#ifndef VERSION_H
+	#define VERSION_H
+
+#ifndef  VERSION_MAJOR
+	#define VERSION_MAJOR 0 
+#endif /* end of define VERSION_MAJOR */
+
+#ifndef VERSION_MINOR
+	#define VERSION_MINOR 1
+#endif /* end of define VERSION_MINOR */
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* end of include guard: RDMA_COMMON_H */
