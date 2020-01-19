d281742b3bd6aa3cbf3958a3e3cfaeff63b692fb
liao20081228 <liao20081228@126.com>
2020-01-19 17:09:40 +0800

    2020年 01月 19日 星期日 17:09:40 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/rdmacm/async/client.c
+++ rdma/rdmacm/async/client.c
@@ -2,11 +2,24 @@
 #include<stdlib.h>
 #include<memory.h>
 #include<string.h>
+#include<errno.h>
 #include<rdma/rdma_cma.h>
 #include<rdma/rdma_verbs.h>
+#include<pthread.h>
+#include<semaphore.h>
 
 int main(void)
 {
-	struct rdma_event_channel* rdma_event_channel=
+
+
+	struct rdma_event_channel* chanel_event=rdma_create_event_channel();
+	if(!chanel_event)
+	{
+		perror("create event channel failed");
+		return 1;
+	}
+
+	
+
 	return 0;
 }
