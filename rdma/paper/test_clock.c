4c710d000a6fd9c9ef7edf34588076bf6e83f064
liao20081228 <liao20081228@126.com>
2020-01-19 21:21:42 +0800

    2020年 01月 19日 星期日 21:21:42 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/paper/test_clock.c
+++ rdma/paper/test_clock.c
@@ -1,8 +1,13 @@
 #include"get_clock.h"
-
+#include<unistd.h>
+#include<stdio.h>
 int main(void)
 {
-	double cput_freq=get_cpu_mhz(0); 
-	clock_t c1=
+	double cpu_freq=get_cpu_mhz(0); 
+	cycles_t c1, c2;
+	c1=get_cycles();
+	sleep(1);
+	c2=get_cycles();
+	printf("1s= %E us", (c2 - c1 ) / cpu_freq );
 	return 0;
 }
