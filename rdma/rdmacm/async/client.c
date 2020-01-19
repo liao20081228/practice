5f704bf78e8bc04db4c70b0c76e9aa442553e08e
liao20081228 <liao20081228@126.com>
2020-01-19 17:03:38 +0800

    2020年 01月 19日 星期日 17:03:38 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

---
+++ rdma/rdmacm/async/client.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<memory.h>
+#include<string.h>
+#include<rdma/rdma_cma.h>
+#include<rdma/rdma_verbs.h>
+
+int main(void)
+{
+	struct rdma_event_channel* rdma_event_channel=
+	return 0;
+}
