7a3c26b35afb58f40d73ba7e279c8f7010555de3
liao20081228 <liao20081228@126.com>
2020-01-20 18:39:44 +0800

    2020年 01月 20日 星期一 18:39:44 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/ibverbs/list_ib_dev.c
+++ rdma/ibverbs/list_ib_dev.c
@@ -1,10 +1,13 @@
 #include<infiniband/verbs.h>
-
+#include<stdio.h>
 int main(int argc, char* argv[])
 {
-	struct ibv_device** dev_list = ibv_get_device_list(NULL);
-
-	for (struct ibv_device * i = )
+	int dev_num = 0;
+	struct ibv_device** dev_list = ibv_get_device_list(&dev_num);
+	for(int i = 0; i < dev_num ; ++i)
+	{
+		printf("%s, %llx", ibv_get_device_name(dev_list[i]), ibv_get_device_guid(dev_list[i]));
+	}
 
 	return 0;	
 }
