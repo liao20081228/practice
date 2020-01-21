324652219ec4f237fee200b655501e419d8b6420
liao20081228 <liao20081228@126.com>
2020-01-21 18:57:52 +0800

    2020年 01月 21日 星期二 18:57:52 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/rdmacm/rsocket/server.c
+++ rdma/rdmacm/rsocket/server.c
@@ -9,8 +9,17 @@
 struct rdma_addrinfo hint, res;
 static char *src_addr;
 static char *dst_addr;	
-static const char * port = "10000"; 
+static const char *port = "10000"; 
 static struct timespec start = {0, 0} , end = {0, 0};
+static int iteration = 1000;
+static int transfer_size = 1000;
+static int transfer_count = 1000;
+static int buffer_size = 0;
+static int inline_size = 64; 
+static int keepalive = 0;
+static int flags = MSG_DONTWAIT;
+static int poll_timeout = 0;
+static void *buf = NULL;
 
 int main(int argc, char *argv[])
 {
