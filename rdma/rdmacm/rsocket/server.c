6c1ac00f3e4a6014f5df43ac064e30d17c8a151a
liao20081228 <liao20081228@126.com>
2020-01-21 18:51:44 +0800

    2020年 01月 21日 星期二 18:51:44 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/rdmacm/rsocket/server.c
+++ rdma/rdmacm/rsocket/server.c
@@ -4,13 +4,13 @@
 #include<stdlib.h>
 #include<endian.h>
 #include<sys/socket.h>
-
+#include<time.h>
 
 struct rdma_addrinfo hint, res;
 static char *src_addr;
 static char *dst_addr;	
 static const char * port = "10000"; 
-static struct timeval start, end;
+static struct timespec start = {0, 0} , end = {0, 0};
 
 int main(int argc, char *argv[])
 {
