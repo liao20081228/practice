a4ee56b0fd15ace2cd73d4b0e4badab73c2e7d77
liao20081228 <liao20081228@126.com>
2020-02-09 17:51:41 +0800

    2020年 02月 09日 星期日 17:51:41 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- linux_programing/multiple_thread/create_and_exit_thread.c
+++ linux_programing/multiple_thread/create_and_exit_thread.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
 #include<pthread.h>
-
+#include<unistd.h>
 void* add(void * cnt)
 {
 	int * count = (int *)cnt;
 	for(int i = 0; i < 20; ++i,(*count)++)
-		printf("%lu, count is %d", pthread_self(),*count);
-	if (*count == 20)
+	{
+		printf("%lu, count is %d\n", pthread_self(),*count);
+		sleep(1);
+	}
+		if (*count == 20)
 		pthread_exit((void*)20);
 	return (void*)(*count);
 }
@@ -17,18 +20,24 @@ int main()
 {
 	int counter = 0;
 
-	pthread_t thread_id = 0;
+	pthread_t thread_id1 = 0, thread_id2=0;
 
-	if(pthread_create(&thread_id, NULL, add, &counter))
+	if(pthread_create(&thread_id1, NULL, add, &counter))
 	{
 		perror("create thread failed!");
 		return 1;
 	}
-	if(pthread_create(&thread_id, NULL, add, &counter))
+	if(pthread_create(&thread_id2, NULL, add, &counter))
 	{
 		perror("create thread failed!");
 		return 1;
 	}
+	int ret1 = 0,  ret2 = 0; 
+	pthread_join(thread_id1,(void**)&ret1);
+	pthread_join(thread_id2,(void**)&ret2);
 
+	printf("%lu, ret1 is %d", thread_id1, ret1);
 
+	printf("%lu, ret2 is %d", thread_id2, ret2);
+	return 0;
 }
