f2c542a6a2deaf5839535455b0370b96b26364f3
liao20081228 <liao20081228@126.com>
2020-02-09 18:45:39 +0800

    2020年 02月 09日 星期日 18:45:39 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- linux_programing/multiple_thread/pthread_cleanup_push_and_pop.c
+++ linux_programing/multiple_thread/pthread_cleanup_push_and_pop.c
@@ -10,11 +10,25 @@ void* fun(void * arg)
 {
 	pthread_cleanup_push(clean, (void*)1);
 	pthread_cleanup_push(clean, (void*)2);
-	pthread_cleanup_push(clean, (void*)2);
-	pthread_cleanup_push(clean, (void*)2);
+	pthread_cleanup_push(clean, (void*)3);
+	pthread_cleanup_push(clean, (void*)4);
 	sleep(2);
+	pthread_cleanup_pop(0);//正常退出时不执行，异常退出执行
 	pthread_cleanup_pop(0);
+	pthread_cleanup_pop(1);//正常退出执行，异常退出执行
 	pthread_cleanup_pop(1);
-	pthread_cleanup_pop(0);
-	pthread_cleanup_pop(0);
+	return NULL;
+}
+
+int main()
+{
+	pthread_t id = 0;
+
+	pthread_create(&id, NULL, fun, NULL);
+	pthread_join(id, NULL);
+
+	pthread_create(&id, NULL, fun, NULL);
+	pthread_cancel(id);
+	pthread_join(id, NULL);
+	return 0;
 }
