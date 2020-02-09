56dce4db913d93e735d38ab03bd4b4461ec3cc97
liao20081228 <liao20081228@126.com>
2020-02-09 15:57:38 +0800

    2020年 02月 09日 星期日 15:57:38 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/rdmacm/async/client.c
+++ rdma/rdmacm/async/client.c
@@ -18,32 +18,4 @@
 int main(void)
 {
 
-	pthread_t productor,consumer;
-
-
-	struct rdma_event_channel* cm_channel = rdma_create_event_channel();
-	if(!cm_channel)
-	{
-		perror("create event channel failed");
-		return FAILURE;
-	}
-
-	struct rdma_cm_id *cm_id = NULL;
-	int ret = rdma_create_id(cm_channel, &cm_id, NULL, RDMA_PS_TCP);
-	if (ret)
-	{
-		perror("create cm id failed");
-		goto free_cm_channel;
-	}
-
-	
-	
-
-
-
-free_cm_channel:
-	rdma_destroy_event_channel(cm_channel);
-	return FAILURE;
-
-	return SUCCESS;
 }
