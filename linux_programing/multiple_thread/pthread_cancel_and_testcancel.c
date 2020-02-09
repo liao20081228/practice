eea955ad539faab51f2f5d3ff6c7cb79a70841f3
liao20081228 <liao20081228@126.com>
2020-02-09 18:57:39 +0800

    2020年 02月 09日 星期日 18:57:39 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- linux_programing/multiple_thread/pthread_cancel_and_testcancel.c
+++ linux_programing/multiple_thread/pthread_cancel_and_testcancel.c
@@ -4,8 +4,9 @@
 
 void * fun1(void* arg)
 {
-	printf("before sleep\n");
-	printf("after sleep\n");
+//	printf("before sleep\n");
+	int a=10;
+//	printf("after sleep\n");
 	return NULL;
 }
 
@@ -13,7 +14,7 @@ int main()
 {
 	pthread_t id =0;
 	pthread_create(&id, NULL, fun1, NULL);
-	if(pthread_cancel(id))
+	if(0!=pthread_cancel(id))
 		printf("cancel faild\n");
 	pthread_join(id, NULL);
 }
