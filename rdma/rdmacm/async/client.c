e3ae5fc09dc1867e1900419409c32da0b5ab36b9
liao20081228 <liao20081228@126.com>
2020-02-09 16:10:15 +0800

    2020年 02月 09日 星期日 16:10:15 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/rdmacm/async/client.c
+++ rdma/rdmacm/async/client.c
@@ -14,8 +14,20 @@
 #endif
 
 
+struct context
+{
+	struct rdma_event_channel * rdma_chnnel;
+	struct rdma_cm_id* rdma_id;
+	struct sockaddr src_addr;
+	struct sockaddr dst_addr;
+};
+
+
 
 int main(void)
 {
-
+	struct rdma_event_channel * rdma_chnnel = rdma_create_event_channel();
+	struct rdma_cm_id* rdma_id = NULL;
+	rdma_create_id(rdma_chnnel,&rdma_id,NULL,RDMA_PS_TCP);
+	rdma_resolve_addr(rdma_id, NULL, )
 }
