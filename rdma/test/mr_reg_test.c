493fa8b4f3f98ccedc8b104234ab8810ca6dcffd
liao20081228 <liao20081228@126.com>
2020-02-10 10:27:39 +0800

    2020年 02月 10日 星期一 10:27:39 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/test/mr_reg_test.c
+++ rdma/test/mr_reg_test.c
@@ -1,2 +1,13 @@
 #include<stdio.h>
+#include"get_clock.h"
+#include<rdma/rdma_cma.h>
+#include<rdma/rdma_verbs.h>
+#include<infiniband/verbs.h>
+int main()
+{
+	cycles_t reg_s[100]= {0}, reg_e[100]={0};
+	cycles_t dereg_s[100]= {0}, dereg_e[100]={0};
+	
+	
+}
 
