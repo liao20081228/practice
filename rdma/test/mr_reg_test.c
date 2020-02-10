f46c90f783fa7d1f383018705703b1e5ee3140b2
liao20081228 <liao20081228@126.com>
2020-02-10 11:03:41 +0800

    2020年 02月 10日 星期一 11:03:41 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/test/mr_reg_test.c
+++ rdma/test/mr_reg_test.c
@@ -13,9 +13,10 @@ int main()
 	struct ibv_device ** dev_list=ibv_get_device_list(NULL);
 	struct ibv_context* context= ibv_open_device(dev_list[0]);
 	ibv_free_device_list(dev_list);
+	struct ibv_pd* pd=ibv_alloc_pd(context);	
 
 
-
+	printf("mr size\tmax_reg\tmin_reg\tmean_reg\tmax_dereg\tmin_dereg\tmean_dereg\n");
 	for(int k = 1; k<=64*1024*1024;k*=2)
 	{
 		for(int i=0; i< 100;++i)
@@ -44,7 +45,8 @@ int main()
 		}
 		mean_reg=sum_reg/100;
 		mean_dereg=sum_dereg/100;
-	
+		printf("%d\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\n",k,max_reg,min_reg,mean_reg,
+							max_dereg,min_dereg,mean_dereg);
 	}
 }
 
