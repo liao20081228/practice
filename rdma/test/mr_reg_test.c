51643adb68787eefb2341a5873104cb3fb4b7471
liao20081228 <liao20081228@126.com>
2020-02-10 10:33:38 +0800

    2020年 02月 10日 星期一 10:33:38 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/test/mr_reg_test.c
+++ rdma/test/mr_reg_test.c
@@ -5,9 +5,22 @@
 #include<infiniband/verbs.h>
 int main()
 {
-	cycles_t reg_s[100]= {0}, reg_e[100]={0};
-	cycles_t dereg_s[100]= {0}, dereg_e[100]={0};
+	cycles_t start= 0, end=0;
+	double mzh=0;
+	double reg[100]={0},max_reg=0,min_reg=0,sum_reg=0,mean_reg=0;
+	double dereg[100]={0},max_dereg=0,min_dereg=0,sum_dereg=0,mean_dereg=0;
+
+	while (1)	
+	{
+		for(int i=0; i< 100;++i)
+		{
+			mhz=get_cpu_mhz(0);
+			start=get_cycles();
+			
+			end=get_cycles();
+				
+		}
 	
-	
+	}
 }
 
