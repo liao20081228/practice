be3b0a721200e7e8eb116169c53fbd7d9a993385
liao20081228 <liao20081228@126.com>
2020-02-10 10:39:39 +0800

    2020年 02月 10日 星期一 10:39:39 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/test/mr_reg_test.c
+++ rdma/test/mr_reg_test.c
@@ -6,7 +6,7 @@
 int main()
 {
 	cycles_t start= 0, end=0;
-	double mzh=0;
+	double mhz=0;
 	double reg[100]={0},max_reg=0,min_reg=0,sum_reg=0,mean_reg=0;
 	double dereg[100]={0},max_dereg=0,min_dereg=0,sum_dereg=0,mean_dereg=0;
 
@@ -16,10 +16,27 @@ int main()
 		{
 			mhz=get_cpu_mhz(0);
 			start=get_cycles();
-			
+			//reg			
 			end=get_cycles();
-				
+			reg[i]=(end-start)/mhz;
+			sum_reg+=reg[i];
+			if(max_reg<reg[i])
+				max_reg=reg[i];
+			if(min_reg>reg[i])
+				min_reg=reg[i];	
+			mhz=get_cpu_mhz(0);
+			start=get_cycles();
+			//dereg	
+			end=get_cycles();
+			reg[i]=(end-start)/mhz;
+			sum_reg+=reg[i];
+			if(max_reg<reg[i])
+				max_reg=reg[i];
+			if(min_reg>reg[i])
+				min_reg=reg[i];		
+
 		}
+		min_reg=sum_reg/100;
 	
 	}
 }
