6b11b89b28020c6851216d9cd2e9f435f55bad32
liao20081228 <liao20081228@126.com>
2020-02-23 21:58:11 +0800

    2020年 02月 23日 星期日 21:58:11 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/paper/mem_pool.c
+++ rdma/paper/mem_pool.c
@@ -17,10 +17,19 @@ struct freelist
 struct mem_pool
 {
 	struct freelist free2;
+	struct freelist free4;
+	struct freelist free6;
+	struct freelist free8;
+	struct freelist free16;
+	struct freelist free32;
+	struct freelist free64;
 };
 
 
-
+void* my_malloc(int n)
+{
+	
+}
 
 
 
