be6e90953da98528ee08d5f7a8eb939d7f024d95
liao20081228 <liao20081228@126.com>
2020-02-24 13:45:40 +0800

    2020年 02月 24日 星期一 13:45:40 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/paper/mem_pool.c
+++ rdma/paper/mem_pool.c
@@ -2,13 +2,18 @@
 #include<pthread.h>
 #include<math.h>
 
+struct real_mem
+{
+	void* addr;
+	int length;
+	struct real_mem*next;
+};
 
 
 struct mempool
 {
+	struct real_mem realbuf;
 	
-	unsigned char bitmap[]
-
 };
 
 
