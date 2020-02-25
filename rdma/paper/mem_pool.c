cdefb468eee512aee125947ae75157e72c7704ba
liao20081228 <liao20081228@126.com>
2020-02-25 09:27:31 +0800

    2020年 02月 25日 星期二 09:27:31 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/paper/mem_pool.c
+++ rdma/paper/mem_pool.c
@@ -6,6 +6,7 @@ struct real_mem
 {
 	void* addr;
 	int length;
+	int ref;
 	struct real_mem*next;
 };
 
@@ -27,3 +28,19 @@ struct mempool
 };
 
 
+
+
+int create_mem_pool(struct mem_pool *pool)
+{
+	
+}
+
+
+
+
+int main()
+{
+	struct mem_pool* pool = (struct mem_pool*)malloc(sizeof mem_pool);
+	
+	create_mem_pool(pool);	
+}
