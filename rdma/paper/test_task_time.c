3c04651b060228848519ba0ec4a830251675728b
liao20081228 <liao20081228@126.com>
2020-02-27 10:58:48 +0800

    2020年 02月 27日 星期四 10:58:48 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/paper/test_task_time.c
+++ rdma/paper/test_task_time.c
@@ -1,8 +1,24 @@
 #include<stdio.h>
 #include"get_clock.h"
 
-int main()
-
+struct data
 {
-	int a[28*500*1]=
+	short int nodeid;
+	unsigned char senerid;
+	unsigned char varid;
+	unsigned long long int time;
+	unsigned long long int number;
+	double data;
+};
+
+int main()
+{
+	printf("%lu\n",sizeof(char));
+	printf("%lu\n",sizeof(short));
+	printf("%lu\n",sizeof(int));
+	printf("%lu\n",sizeof(long int));
+	printf("%lu\n",sizeof(long long int));
+	printf("%lu\n",sizeof(float));
+	printf("%lu\n",sizeof(double ));
+	printf("%lu\n",sizeof(long double));
 }
