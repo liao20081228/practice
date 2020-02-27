b88738d491757e7857ec0c81aa177a9b9b751644
liao20081228 <liao20081228@126.com>
2020-02-27 18:40:42 +0800

    2020年 02月 27日 星期四 18:40:42 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/paper/test_task_time.c
+++ rdma/paper/test_task_time.c
@@ -14,18 +14,31 @@ struct test
 int main()
 {
 	struct test a[4080]={0};
-	for(int i = 0; i < 4080;i++)
+	for(int i = 0; i < 4000;i++)
 	{	
 		a[i].nodeid=3;
 		a[i].time = i;
 		a[i].number=i;
-
+		a[i].kind=1;
 		for(int m=0;i<7;i++)
 		{
 			a[i].senerid=m;
 			a[i].data=3.123456+m;
 		}
 	}
+	for(int i = 4000; i < 4080;i++)
+	{	
+		a[i].nodeid=3;
+		a[i].time = i;
+		a[i].number=i;
+		a[i].kind=2;
+		for(int m=0;i<7;i++)
+		{
+			a[i].senerid=m;
+			a[i].data=3.123456+m;
+		}
+	}
+
 	double mhz= get_cpu_mhz(0);
 	cycles_t s=get_cycles();
 	double arg_500_1;
@@ -36,9 +49,12 @@ int main()
 	double arg_500_6;
 	double arg_500_7;
 	double arg_10_1;
-	double arg_10_1;
 	double arg_10_2;
 	double arg_10_3;
+	double arg_10_4;
+	double arg_10_5;
+	double arg_10_6;
+	double arg_10_7;
 	for(int i=0; i<4080;i++)
 	{
 		switch(a[i].nodeid)
