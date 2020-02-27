85be29be54b295616d5ed4fc8ed60c8a3c6df912
liao20081228 <liao20081228@126.com>
2020-02-27 11:16:54 +0800

    2020年 02月 27日 星期四 11:16:54 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/paper/test_task_time.c
+++ rdma/paper/test_task_time.c
@@ -5,6 +5,7 @@ struct test
 {
 	short int nodeid;
 	unsigned char senerid;
+	unsigned char kind;
 	unsigned long long int time;
 	unsigned long long int number;
 	double data;
@@ -12,18 +13,19 @@ struct test
 
 int main()
 {
-	struct test a[500*8*8]={0};
-	for(int i = 0; i < 500*8*8;i++)
+	struct test a[500*8]={0};
+	for(int i = 0; i < 500*8;i++)
 	{	
 		a[i].nodeid=1;
 		a[i].time = i;
 		a[i].number=i;
+
 		for(int m=0;i<7;i++)
 		{
 			a[i].senerid=m;
+			a[i].data=3.123456+m;
 		}
-
-
 	}
+	double mhz= get_cpu_mhz(0);
 
 }
