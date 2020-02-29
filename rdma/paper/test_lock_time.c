9d483cd61dd05d60b19155b4a866150c7510a523
liao20081228 <liao20081228@126.com>
2020-02-29 10:10:40 +0800

    2020年 02月 29日 星期六 10:10:40 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/paper/test_lock_time.c
+++ rdma/paper/test_lock_time.c
@@ -1,9 +1,42 @@
 #include<pthread.h>
 #include"get_clock.h"
 
+
+
+void* fun(void*arg)
+{
+	double mean=0;
+	pthread_mutex_t* mutex=(pthread_mutex_t*)arg;
+	int n=1;
+	int k=10000;
+	cycles_t s=0;
+	cycles_t e=0;
+	for(int i = 0; i<1000;i++)
+	{
+		s=get_cycles();
+		pthread_mutex_lock(mutex);
+		n=n+1;
+		if(n==k)
+			break;
+		pthread_mutex_unlock(mutex);
+	}
+}
+
+
+
+
+
+
+
+
 int main()
 {
 	pthread_mutex_t mutex=PTHREAD_MUTEX_INITIALIZER;
 	pthread_spinlock_t spin;
 	pthread_spin_init(&spin, PTHREAD_PROCESS_PRIVATE);
+
+
+
+
+
 }
