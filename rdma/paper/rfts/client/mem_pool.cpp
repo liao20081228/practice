db416fb192d6dd40dba874e59ae2b46e214429ed
liao20081228 <liao20081228@126.com>
2020-02-29 12:30:31 +0800

    2020年 02月 29日 星期六 12:30:31 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/paper/rfts/client/mem_pool.cpp
+++ rdma/paper/rfts/client/mem_pool.cpp
@@ -1,6 +1,6 @@
 #include"mem_pool.hpp"
 
-rfts::mempool::mempool(int afreq, int tfreq, int size, int tou, int nodenum):
+rfts::mempool::mempool(int afreq, int tfreq, int size, int sensor_num, int node_num, int kind):
 	addr(new char[afreq / tfreq * size * kind* nodenum * 2]()),length(afreq/tfreq*2)
 {
 }
