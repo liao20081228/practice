24176668dc6fd8b7052b25bceac9da05e9224ee3
liao20081228 <liao20081228@126.com>
2020-02-29 08:52:33 +0800

    2020年 02月 29日 星期六 08:52:33 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/paper/rfts/client/mem_pool.hpp
+++ rdma/paper/rfts/client/mem_pool.hpp
@@ -2,11 +2,13 @@
 #include<mutex>
 namespace rfst
 {
+	using std::mutex;
 	class mempool
 	{
 	private:
 		void* addr;
 		int length;
+		mutex suo;
 	public:
 		mempool(int length);
 		void* malloc(int n);
