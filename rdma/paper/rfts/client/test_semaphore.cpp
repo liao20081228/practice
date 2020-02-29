d2204240448181db06f376f7f0c8a7f9109196e1
liao20081228 <liao20081228@126.com>
2020-03-01 00:34:58 +0800

    2020年 03月 01日 星期日 00:34:58 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/paper/rfts/client/test_semaphore.cpp
+++ rdma/paper/rfts/client/test_semaphore.cpp
@@ -1,15 +1,29 @@
 #include"semaphore.hpp"
 #include<thread>
-
+#include<iostream>
 int fun(semaphore& sem)
 {
-
+	while(1)
+	{
+		sem.wait();
+		std::cout<<sem.getvalue()<<std::endl;
+	}
 }
 
 
 int main()
 {
-	std::thread a();
+	semaphore sem;
+	std::thread a(fun, std::ref(sem));
+	std::thread b(fun, std::ref(sem));
+	std::thread c(fun, std::ref(sem));
+	std::thread d(fun, std::ref(sem));
+	for(int i = 0;i < 100;++i)
+		sem.post();
+	a.join();
+	b.join();
+	c.join();
+	d.join();
 }
 
 
