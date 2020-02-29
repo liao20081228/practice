557f92954649d069f234a463fa7e16a3deefe4bc
liao20081228 <liao20081228@126.com>
2020-02-29 22:05:09 +0800

    2020年 02月 29日 星期六 22:05:09 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/paper/rfts/client/semaphore.cpp
+++ rdma/paper/rfts/client/semaphore.cpp
@@ -4,11 +4,11 @@ semaphore::semaphore(const int pshared, const unsigned int value):sem(new sem_t)
 {
 	if(sem_init(sem, pshared, value))
 	{
+		delete sem;
 		if(errno == EINVAL)
-		{
-			delete sem;
-			throw std::()
-		}
+			throw std::invalid_argument("initial value exceeds SEM_VALUE_MAX");
+		if(errno == ENOSYS)
+			throw std::
 	}
 
 }
