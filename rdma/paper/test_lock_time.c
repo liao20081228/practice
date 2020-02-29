bfb8710d7151ec82d6cc82c8546da3a7111a522e
liao20081228 <liao20081228@126.com>
2020-02-29 10:16:41 +0800

    2020年 02月 29日 星期六 10:16:41 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/paper/test_lock_time.c
+++ rdma/paper/test_lock_time.c
@@ -1,6 +1,6 @@
 #include<pthread.h>
 #include"get_clock.h"
-
+#include<stdio.h>
 
 
 void* fun(void*arg)
@@ -9,19 +9,49 @@ void* fun(void*arg)
 	pthread_mutex_t* mutex=(pthread_mutex_t*)arg;
 	int n=1;
 	int k=10000;
+	double mhz=0;
 	cycles_t s=0;
 	cycles_t e=0;
 	for(int i = 0; i<1000;i++)
 	{
+		mhz=get_cpu_mhz(0);
 		s=get_cycles();
 		pthread_mutex_lock(mutex);
 		n=n+1;
 		if(n==k)
 			break;
 		pthread_mutex_unlock(mutex);
+		e=get_cycles();
+		mean=mean+(e-s)/mhz*1000;
 	}
+	printf("mutex mean %lf\n", mean/1000);
+	return NULL;
 }
 
+void* fun1(void*arg)
+{
+	double mean=0;
+	pthread_spinlock_t* mutex=(pthread_spinlock_t*)arg;
+	int n=1;
+	int k=10000;
+	double mhz=0;
+	cycles_t s=0;
+	cycles_t e=0;
+	for(int i = 0; i<1000;i++)
+	{
+		mhz=get_cpu_mhz(0);
+		s=get_cycles();
+		pthread_spin_lock(mutex);
+		n=n+1;
+		if(n==k)
+			break;
+		pthread_spin_unlock(mutex);
+		e=get_cycles();
+		mean=mean+(e-s)/mhz*1000;
+	}
+	printf("pthread_spin  mean %lf\n", mean/1000);
+	return NULL;
+}
 
 
 
