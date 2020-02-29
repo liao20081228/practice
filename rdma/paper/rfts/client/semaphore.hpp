aeb50fa05673eeda2ff75d60b8e8b88be20f601e
liao20081228 <liao20081228@126.com>
2020-02-29 20:46:46 +0800

    2020年 02月 29日 星期六 20:46:46 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/paper/rfts/client/semaphore.hpp
+++ rdma/paper/rfts/client/semaphore.hpp
@@ -2,19 +2,16 @@
 #ifndef RFTS_SEMAPHORE_H
 #define RFTS_SEMAPHORE_H
 #include<semaphore.h>
-
 class semaphore
 {
 	private:
 		sem_t sem;
 	public:
-		explicit semaphore(void) noexcept;
+		explicit semaphore(const int psharedi = 0, const unsigned int value = 0) noexcept;
 		~semaphore(void) noexcept;
 		void post(void) noexcept;
 		void wait(void) noexcept;
 };
-
-
 
 
 
