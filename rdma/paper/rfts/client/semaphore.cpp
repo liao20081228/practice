838b2305f4bdc2a6943e855f3ad3c6e6bc257d4d
liao20081228 <liao20081228@126.com>
2020-02-29 23:47:05 +0800

    2020年 02月 29日 星期六 23:47:05 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/paper/rfts/client/semaphore.cpp
+++ rdma/paper/rfts/client/semaphore.cpp
@@ -47,6 +47,17 @@ semaphore::semaphore(semaphore&& ref):sem(ref.sem),name(ref.name)
 	ref.sem = nullptr;
 }
 
+semaphore& semaphore::operator = (semaphore&& ref)
+{
+	if(this == &ref)
+		return *this;
+	sem = ref.sem;
+	name = ref.name;
+	ref.name = nullptr;
+	ref.sem = nullptr;
+	return *this; 
+}
+
 
 semaphore::~semaphore(void)
 {
