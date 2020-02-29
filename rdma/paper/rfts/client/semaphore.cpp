ef5ef9ffdd351774fb45acf39da4809e073673c1
liao20081228 <liao20081228@126.com>
2020-02-29 22:10:54 +0800

    2020年 02月 29日 星期六 22:10:54 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/paper/rfts/client/semaphore.cpp
+++ rdma/paper/rfts/client/semaphore.cpp
@@ -8,7 +8,12 @@ semaphore::semaphore(const int pshared, const unsigned int value):sem(new sem_t)
 		if(errno == EINVAL)
 			throw std::invalid_argument("initial value exceeds SEM_VALUE_MAX");
 		if(errno == ENOSYS)
-			throw std::
+			throw std::system_error(errno, "system not support");
 	}
 
+}
+
+semaphore::semaphore(const char* name, int oflag, mode_t mode, unsigned int value)
+{
+	
 }
--- rdma/paper/rfts/client/semaphore.hpp
+++ rdma/paper/rfts/client/semaphore.hpp
@@ -6,16 +6,16 @@
 #include<sys/stat.h>
 #include<cerrno>
 #include<stdexcept>
+#include<system_error>
 class semaphore
 {
 	private:
 		sem_t* const sem;
 		bool name;
 	public:
-		explicit semaphore(const int pshared = 0, const unsigned int value = 0);
-		explicit semaphore(const char* name, int oflag = O_RDWR | O_CREAT);
-		explicit semaphore(const char* name, const int oflag = O_RDWR | O_CREAT, 
-				const mode_t mode = 00666, const unsigned int value = 0);
+		explicit semaphore(int pshared = 0,  unsigned int value = 0);
+		explicit semaphore(const char* name, int oflag = O_RDWR | O_CREAT, 
+				 mode_t mode = 00666, unsigned int value = 0);
 		~semaphore(void);
 		void post(void);
 		void wait(void);
