b59e10feb64df85b4bb636eff3d2b2216d3105d3
liao20081228 <liao20081228@126.com>
2020-02-29 14:48:31 +0800

    2020年 02月 29日 星期六 14:48:31 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/paper/rfts/client/main.cpp
+++ rdma/paper/rfts/client/main.cpp
@@ -1,6 +1,7 @@
 #include"trans_args.hpp"
 #include"mem_pool.hpp"
 #include<iostream>
+#include"get_clock.h"
 int main()
 {
 	using namespace rfts;
@@ -19,9 +20,24 @@ int main()
 
 	void * addr = nullptr;
 	mempool.rfree();
-	for(int i = 0; ;i++)
+	double mean1 = 0;
+	double mean2 = 0;
+	cycles_t s=0;
+	cycles_t e=0;
+	double mhz = 0;
+	for(int i=0; i<10000;i++)
 	{
-		addr = mempool.rmalloc();
+		mhz=get_cpu_mhz(0);
+		s=get_cycles();
+		addr=mempool.rmalloc();
+		e=get_cycles();
+		mean1= mean1 + ( e-s ) / mhz * 1000;
+		mhz=get_cpu_mhz(0);
+		s=get_cycles();
+		mempool.rfree();
+		e=get_cycles();
+		mean2= mean2 + ( e-s ) / mhz * 1000;
 	}
+	std::cout << "malloc mean:" << mean1/10000 << "," << "free mean:"<< mean2/10000<<std::endl;
 	return 0;
 }
