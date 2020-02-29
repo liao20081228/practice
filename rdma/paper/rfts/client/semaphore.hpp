4f8f25998beba829ce3b77198e0acc86678867e2
liao20081228 <liao20081228@126.com>
2020-02-29 21:05:48 +0800

    2020年 02月 29日 星期六 21:05:48 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/paper/rfts/client/semaphore.hpp
+++ rdma/paper/rfts/client/semaphore.hpp
@@ -6,8 +6,12 @@ class semaphore
 {
 	private:
 		sem_t *sem;
+		bool name;
 	public:
 		explicit semaphore(const int pshared = 0, const unsigned int value = 0);
+		explicit semaphore(const char* name, int oflag);
+		explicit semaphore(const char* name, const int oflag, 
+				const mode_t mode, const unsigned int value);
 		~semaphore(void);
 		void post(void);
 		void wait(void);
