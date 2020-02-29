348412e9038898b312288771233520d72c9b008d
liao20081228 <liao20081228@126.com>
2020-02-29 12:48:32 +0800

    2020年 02月 29日 星期六 12:48:32 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/paper/rfts/client/mem_pool.hpp
+++ rdma/paper/rfts/client/mem_pool.hpp
@@ -19,19 +19,15 @@ namespace rfts
 		void* const addr;//原始地址
 		const int length;//长度
 
-		int elesize;//每个元素的大小
+		const int elesize;//每个元素的大小
+		const int capacity;//队伍容量
 		int front;//队首指针
 		int rear;//队尾指针
-		const int capacity;//队伍容量
-		int full;//队满
-#ifndef RFTS_CLIENT
-		mutex mutext_mem_pool;
-#endif
 	public:
-		mempool(const trans_args& transargs);
+		mempool(const trans_args& transargs) noexcept;
 		void* malloc(int) noexcept;
 		int  free(void*) noexcept;
-		const void*  get_real_addr() const;
+		const void*  get_real_addr() const noexcept;
 	};
 }
 
