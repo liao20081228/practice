f9fa4487768b21c5e538e362e7b38a94dfa354ef
liao20081228 <liao20081228@126.com>
2020-03-01 06:50:13 +0000

    Sun Mar  1 06:50:13 UTC 2020
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/paper/rfts/client/trans_args.hpp
+++ rdma/paper/rfts/client/trans_args.hpp
@@ -6,23 +6,21 @@ namespace
 {
 struct data//数据结构
 {
-	short    int nodeid;    //节点ID
+	unsigned short int nodeid;    //节点ID
 	unsigned char sensorid; //传感器ID
 	unsigned char type;	//数据类型,束流位置？长度？
 	struct   timespec time; //时间
 	double   value;		//值
 };
 
-
-
 struct trans_args
 {
-	int afreq;//采集平率
-	int tfreq;//传输频率
-	int size;//每个物理量数据的大小
-	int kind;//每个节点要测试几种
-	int sensor_num;//每个节点有几个探头
-	int node_num;//总共多少个节点
+	unsigned int afreq;//采集平率
+	unsigned int tfreq;//传输频率
+	unsigned char size;//每个物理量数据的大小
+	unsigned char  kind;//每个节点要测试几种
+	unsigned char sensor_num;//每个节点有几个探头
+	unsigned short node_num;//总共多少个节点
 };
 
 }
