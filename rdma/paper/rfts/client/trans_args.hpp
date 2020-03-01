9aecca8bd69fea8756f9ee72897bd07f2ee6cc86
liao20081228 <liao20081228@126.com>
2020-03-01 06:32:13 +0000

    Sun Mar  1 06:32:13 UTC 2020
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/paper/rfts/client/trans_args.hpp
+++ rdma/paper/rfts/client/trans_args.hpp
@@ -1,14 +1,16 @@
 #pragma once
 #ifndef RFTS_TRANS_ARGS_H
 #define RFTS_TRANS_ARGS_H
-
-
-struct data
+#include<ctime>
+namespace 
+{
+struct data//数据结构
 {
 	short int nodeid;
 	short int sensorid;
 	short int type;
-
+	struct timespec time;
+	double value;
 };
 
 
@@ -18,10 +20,10 @@ struct trans_args
 	int afreq;//采集平率
 	int tfreq;//传输频率
 	int size;//每个物理量数据的大小
-	 kind;//每个节点要测试几种
+	int kind;//每个节点要测试几种
 	int sensor_num;//每个节点有几个探头
 	int node_num;//总共多少个节点
 };
 
-
+}
 #endif /* end of include guard: RFTS_TRANS_ARG_H */
