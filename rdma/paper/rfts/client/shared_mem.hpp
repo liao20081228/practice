d458a0e0c0364b23c19567f4ebc1902ba134e27d
liao20081228 <liao20081228@126.com>
2020-03-03 04:44:14 +0000

    Tue Mar  3 04:44:14 UTC 2020
    Signed-off-by: liao20081228 <liao20081228@126.com>

---
+++ rdma/paper/rfts/client/shared_mem.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef RFTS_SHARED_MEM_H
+#define RFTS_SHARED_MEM_H
+
+#include<iostream>
+#include<sys/types.h>
+#include<sys/ipc.h>
+#include<sys/shm.h>
+
+class shm
+{
+};
+
+
+#endif /* end of include guard: RFTS_SHARED_MEM_H */
+
+
