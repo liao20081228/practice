9b9fd3cf0a43efa351744b44804febc05a354b9c
liao20081228 <liao20081228@126.com>
2020-03-03 07:56:57 +0000

    Tue Mar  3 07:56:57 UTC 2020
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/paper/rfts/client/semaphore.cpp
+++ rdma/paper/rfts/client/semaphore.cpp
@@ -16,28 +16,8 @@ semaphore::semaphore(const char* name, int oflag, mode_t mode, unsigned int valu
 {
 	if(sem == SEM_FAILED)
 	{
-		switch(errno)
-		{
-			case EACCES:
-				throw std::invalid_argument("user have not permission to accsee");
-			case EEXIST:
-				throw std::invalid_argument("semaphore already exists");
-			case EINVAL:
-				throw std::invalid_argument("initial value exceeds SEM_VALUE_MAX or name invalid");
-			case EMFILE:
-				throw std::system_error(std::error_code(errno, std::system_category()),
-						"per-process limit on  the number of open fd has been reached");
-			case ENAMETOOLONG:
-				throw std::invalid_argument("name was too long");
-			case ENFILE:
-				throw std::system_error(std::error_code(errno, std::system_category()),
-						"system limit the total number of open files has been reached");
-			case ENOENT:
-				throw std::invalid_argument("semaphore no exists and O_CREAT was not specified, or name was bad form");
-			case ENOMEM:
-				throw std::system_error(std::error_code(errno, std::system_category()),
-						"no enough memory"); 
-		}
+		perror("sem_init failed");
+		exit();
 	}
 }
 
