c75b2cde90818df68f8b948d4770ff5dda4e3fe2
liao20081228 <liao20081228@126.com>
2020-03-05 07:38:14 +0000

    Thu Mar  5 07:38:14 UTC 2020
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/paper/rfts/control/client.cpp
+++ rdma/paper/rfts/control/client.cpp
@@ -2,13 +2,17 @@
 
 int main()
 {
-	rfts::transargs ctsas;
-	ctsas.afreq = 500000000;
-	ctsas.tfreq = 10000000;
-	ctsas.size = sizeof(rfts::data);
-	ctsas.kind = 1;
-	ctsas.sensor_num = 8;
-	ctsas.node_num = 1;
+	rfts::transargs tsas;
+	tsas.afreq = 500000000;
+	tsas.tfreq = 10000000;
+	tsas.size = sizeof(rfts::data);
+	tsas.kind = 1;
+	tsas.sensor_num = 8;
+	tsas.node_num = 1;
 
-	pshmem sheme();
+	pshmem shmem("/rfts_pshmem", sysconf(_SC_PAGESIZE), O_RDWR | O_CREAT,
+			REGULAR_FILE_MODE, PROT_WRITE);
+	psem ct_sem("/rfts_psem");
+	shmem.mwrite(&shmem, sizeof(tsas), sizeof(tsas) );
+	tsas<<std::cout;
 }
