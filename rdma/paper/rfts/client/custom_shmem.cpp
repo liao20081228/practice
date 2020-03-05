c5c8d3e24a189640ea7ebb2991821bf39da84876
liao20081228 <liao20081228@126.com>
2020-03-05 03:32:30 +0000

    Thu Mar  5 03:32:30 UTC 2020
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/paper/rfts/client/custom_shmem.cpp
+++ rdma/paper/rfts/client/custom_shmem.cpp
@@ -112,6 +112,9 @@ ssize_t pshmem::read(void* buf, size_t buf_len, size_t nbytes) const noexcept
 	uint64_t temp = cur.load(std::memory_order_acquire);
 	do
 	{
-		memcpy(buf, static_cast<unsigned char*>(addr) + cur, nbytes);
-	}while(1);
+		memcpy(buf, static_cast<unsigned char*>(addr) + cur,
+			temp + nbytes > length ? length - temp : nbytes);
+
+	}while(!cur.compare_exchange_weak(temp,
+		(temp + nbytes > nbytes ? length -1 : cur + nbytes)));
 }
