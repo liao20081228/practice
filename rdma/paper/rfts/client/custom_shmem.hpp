380d0552bac11cc4fd844a005e65d7966c003143
liao20081228 <liao20081228@126.com>
2020-03-05 04:32:14 +0000

    Thu Mar  5 04:32:14 UTC 2020
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/paper/rfts/client/custom_shmem.hpp
+++ rdma/paper/rfts/client/custom_shmem.hpp
@@ -47,6 +47,7 @@ class pshmem
 		pshmem& operator = (const pshmem&& ref) = delete;
 
 		void* getaddr(void ) const noexcept;
+		void clear(void) noexcept;
 		int sync(int flags = MS_SYNC) const noexcept;
 		size_t seek(off_t offset = 0, int whence = SEEK_SET) noexcept;
 		ssize_t read(void* buf, size_t buf_len, size_t nbytes) noexcept;
