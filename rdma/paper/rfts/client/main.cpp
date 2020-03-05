1b15385348b0baba61110f34c64e6a536e597ebe
liao20081228 <liao20081228@126.com>
2020-03-05 06:08:14 +0000

    Thu Mar  5 06:08:14 UTC 2020
    Signed-off-by: liao20081228 <liao20081228@126.com>

---
+++ rdma/paper/rfts/client/main.cpp
@@ -0,0 +1,15 @@
+#include"../common/custom_sem.hpp"
+#include"../common/custom_shmem.hpp"
+#include"../common/trans_args.hpp"
+#include"../common/mem_pool.hpp"
+#include"../common/trans_args.hpp"
+
+#include<thread>
+#include<mutex>
+#include<condition_variable>
+
+
+int main()
+{
+	pshmem shmem;
+}
