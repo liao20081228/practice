0c67b5afcdc38dc95a7a8287a416792f2e991221
liao20081228 <liao20081228@126.com>
2020-03-06 23:10:55 +0800

    2020年 03月 06日 星期五 23:10:55 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/paper/rfts/common/test.cc
+++ rdma/paper/rfts/common/test.cc
@@ -5,7 +5,7 @@ class test
 	int a;
 	int b;
 	public:
-	test(int& a) :a(a), b(a)
+	test(int c) :a(c), b(c)
 	{
 		std::cout<<"constructor" <<std::endl;
 	}
@@ -13,12 +13,44 @@ class test
 	{
 		std::cout<<"move constructor" <<std::endl;
 	}
+	test (const test & ref):a(ref.a), b(ref.b)
+	{
+		  	  std::cout<<"copy constructor" <<std::endl;
+	
+	}
+
+	test & operator = (const test& ref)
+	{
+		a=ref.a;
+		b=ref.b;
+		  	  std::cout<<"copy assign" <<std::endl;
+		return *this;
+	}
+	test & operator = (test&& ref)
+	{
+		a=ref.a;
+		b=ref.b;
+		  	  std::cout<<"move assign" <<std::endl;
+		return *this;
+	}
+
+	~test()
+	{
+		  	  std::cout<<"deconstructor" <<std::endl;
+	}
 };
 
 
 int main(void)
 {
-	int a= 10;
-	test b=test(a);
+	int x= 10;
+	test a(x);
+	std::cout<<"------------\n";
+	test b = test(x);
+	std::cout<<"------------\n";
+	test c(a);
+	std::cout<<"------------\n";
+	a = b;
+	std::cout<<"------------\n";
 	return 0;
 }
