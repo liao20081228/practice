bd3054872ee7f3451ecb4f037258b17540aaf339
liao20081228 <liao20081228@126.com>
2020-03-09 21:40:57 +0800

    2020年 03月 09日 星期一 21:40:57 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/paper/rfts/common/connection_common.cpp
+++ rdma/paper/rfts/common/connection_common.cpp
@@ -4,8 +4,5 @@ rfts::cm_event_channel::cm_event_channel(void) noexcept
 	: cm_channel(rdma_create_event_channel())
 {
 	if (!cm_channel)
-	{
-		errno = EINVAL;
 		PERR("cm_event_channel::rdma_create_event_channel");
-	}
 }
