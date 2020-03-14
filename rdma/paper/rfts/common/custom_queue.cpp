3198841d9b4c3c6c812cefc0790c6606e66db93a
liao20081228 <liao20081228@126.com>
2020-03-14 08:20:13 +0000

    Sat Mar 14 08:20:13 UTC 2020
    Signed-off-by: liao20081228 <liao20081228@126.com>

---
+++ rdma/paper/rfts/common/custom_queue.cpp
@@ -0,0 +1,3 @@
+#include"custom_queue.hpp"
+
+
--- rdma/paper/rfts/common/custom_queue.hpp
+++ rdma/paper/rfts/common/custom_queue.hpp
@@ -7,9 +7,19 @@
 template<typename T>
 class spsc_queue
 {
+private:
 	T* queue;
 	unsigned int __front,__rear;
 	posix_sem __count;
+public:
+	explicit spsc_queue(unsigned int size) noexcept;
+	~spsc_queue(void) noexcept;
+	spsc_queue(const spsc_queue&) = delete;
+	spsc_queue(const spsc_queue&&) = delete;
+	spsc_queue& operator=(const spsc_queue&) = delete;
+	spsc_queue& operator=(spsc_queue&&) = delete;
+	T put(T e) noexcept;
+	T get(T e) noexcept;
 };
 
 
