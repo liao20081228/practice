b6c2497dcc9a6bdc666b073a7cce403afaa41b8c
liao20081228 <liao20081228@126.com>
2020-04-01 09:54:04 +0000

    Wed Apr  1 09:54:04 UTC 2020
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/ibverbs/ib_dev_info.c
+++ rdma/ibverbs/ib_dev_info.c
@@ -4,10 +4,39 @@
 #include<stdlib.h>
 #include<assert.h>
 
+int get_attr(struct ibv_device* device)
+{
+
+
+
+
+
+
+
+
+	return 0;
+}
 
 int main(void)
 {
+	char devname[512] = {0};
+	int  port = 1;
+	
+	ibv_fork_init();
 	int num = 0;
 	struct ibv_device** dev_list = ibv_get_device_list(&num);
-	for
+	if (!dev_list)
+	{
+		perror("ibv_get_device_list");
+		exit(errno);
+	}
+	for (int i = 0; i < num; ++i)
+	{
+		if (get_attr(dev_list[i]))
+		{
+			goto err;
+		}
+	}
+err:
+	ibv_free_device_list(dev_list);
 }
