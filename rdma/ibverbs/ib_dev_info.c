dca6e7b804fd411d949085f7939f1932ab4164a7
liao20081228 <liao20081228@126.com>
2020-04-02 00:14:25 +0800

    2020年 04月 02日 星期四 00:14:25 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/ibverbs/ib_dev_info.c
+++ rdma/ibverbs/ib_dev_info.c
@@ -34,6 +34,7 @@ int get_attr(struct ibv_device* device, int port)
 	for(unsigned int i = 0; i < 4; ++i)
 	{
 		memcpy((uint8_t*)&guid[3-i] + 1,(uint8_t*)&guid_temp + i * 2, 1);
+		memcpy((uint8_t*)&guid[3-i],(uint8_t*)&guid_temp + i * 2 + 1, 1);
 	}
 	sprintf(GUID, "%4X:%4X:%4X:%4X", guid[0],guid[1],guid[2],guid[3]);
 	FPRT(GUID, GUID, s, 30s);
@@ -120,6 +121,7 @@ int main(int argc , char* argv[])
 			get_attr(dev_list[i], port);
 				goto err;
 		}
+
 err:
 	ibv_free_device_list(dev_list);
 }
