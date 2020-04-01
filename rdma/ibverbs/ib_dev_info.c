37e2757a08222b8c3386e59e1a0db700eaee6f84
liao20081228 <liao20081228@126.com>
2020-04-01 23:38:22 +0800

    2020年 04月 01日 星期三 23:38:22 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/ibverbs/ib_dev_info.c
+++ rdma/ibverbs/ib_dev_info.c
@@ -27,7 +27,7 @@ int get_attr(struct ibv_device* device, int port)
 	FPRT(ibv_get_device_name(device), ibv_get_device_name, s, 30s);
 	char* GUID="xxxx:xxxx:xxxx:xxxx";
 	uint64_t guid = ibv_get_device_guid(device);
-	sprintf("%4X%4X:%4X:%4X",)
+	sprintf(guid, "%4X:%4X:%4X:%4X", guid)
 	FPRT(GUID, GUID, s, 30s);
 	
 	struct ibv_context* context = ibv_open_device(device);
