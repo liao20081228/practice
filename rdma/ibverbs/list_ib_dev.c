34376c6ff43911b55644eeea48732d66172834cc
liao20081228 <liao20081228@126.com>
2020-04-05 20:00:16 +0800

    2020年 04月 05日 星期日 20:00:16 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/ibverbs/list_ib_dev.c
+++ rdma/ibverbs/list_ib_dev.c
@@ -3,6 +3,7 @@
 #include<stdlib.h>
 #include<endian.h>
 
+
 int main(void)
 {
 	int dev_num = 0;
@@ -13,11 +14,15 @@ int main(void)
 		perror("ibv_get_device_list failed()");
 		return EXIT_FAILURE;
 	}
+
 	if (!dev_num)
 		fprintf(stdout, "no RDMA device is found\n");
+	
+	fprintf(stdout, "%-24s %-24s %32s %32s, %s, %s ", "device name", "GUID",
+		"node type", "transport layer type", "device path", "ib device path");
+
 	for(int i = 0; i < dev_num ; ++i)
 	{
-		const char *name = ibv_get_device_name;
 	}
 
 	ibv_free_device_list(dev_list);
