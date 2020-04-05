2af828e8abeb5b2c4ccff66dcf41756361b86112
liao20081228 <liao20081228@126.com>
2020-04-05 20:06:16 +0800

    2020年 04月 05日 星期日 20:06:15 CST
    Signed-off-by: liao20081228 <liao20081228@126.com>

--- rdma/ibverbs/list_ib_dev.c
+++ rdma/ibverbs/list_ib_dev.c
@@ -18,9 +18,9 @@ int main(void)
 	if (!dev_num)
 		fprintf(stdout, "no RDMA device is found\n");
 	
-	fprintf(stdout, "%-24s %-24s %32s %32s, %s, %s ", "device name", "GUID",
+	fprintf(stdout, "%-10s %-20s %10s %20s, %50s, %50s\n", "device name", "GUID",
 		"node type", "transport layer type", "device path", "ib device path");
-
+	fprintf(stdout, "------------------------------------------------------------------------------------------------------------------------------------------------------------\n");
 	for(int i = 0; i < dev_num ; ++i)
 	{
 	}
